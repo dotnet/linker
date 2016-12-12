0b663369d6fa70cb364be4c06f6bdc0af0364bda
Sven Boemer <sbomer@gmail.com>
2016-12-12 09:10:21 -0800

    simple empty job

--- netci.groovy
+++ netci.groovy
@@ -9,28 +9,5 @@ def branch = GithubBranchName
 
         def newJob = job(Utilities.getFullJobName(project, os.toLowerCase(), isPR)) {}
 
-        if (os == 'Windows_NT') {
-            newJob.with {
-                steps {
-                    batchFile("build.cmd")
-                }
-            }
-        } else if (os == 'Ubuntu') {
-            newJob.with {
-                steps {
-                    shell("build.sh")
-                }
-            }
-        }
-        
-        Utilities.setMachineAffinity(newJob, os, 'latest-or-auto')
-
-        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-
-        if (isPR) {
-            Utilities.addGithubPRTriggerForBranch(newJob, branch, "${os} Build")
-        } else {
-            Utilities.addGithubPushTrigger(newJob)
-        }
     }
 }
