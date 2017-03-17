9227e483be2b5e18ccc5d4c22ce40cd417bc9bbf
Mike Voorhees <michaelv@unity3d.com>
2017-03-17 10:21:44 -0400

    Clean up try/finally

--- linker/Mono.Linker.Steps/MarkStep.cs
+++ linker/Mono.Linker.Steps/MarkStep.cs
@@ -154,11 +154,9 @@ namespace Mono.Linker.Steps {
 				MethodDefinition method = (MethodDefinition) _methods.Dequeue ();
 				Annotations.Push (method);
 				try {
-					try {
-						ProcessMethod (method);
-					} catch (Exception e) {
-						throw new MarkException (string.Format ("Error processing method: '{0}' in assembly: '{1}'", method.FullName, method.Module.Name), e);
-					}
+					ProcessMethod (method);
+				} catch (Exception e) {
+					throw new MarkException (string.Format ("Error processing method: '{0}' in assembly: '{1}'", method.FullName, method.Module.Name), e);
 				} finally {
 					Annotations.Pop ();
 				}
