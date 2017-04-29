39170b556247bf16fe9e966f4a57c0134dd5bbea
Mike Voorhees <vernish13@gmail.com>
2017-04-29 12:28:53 -0400

    Bring over some newly added test cases

---
+++ linker/Tests/Mono.Linker.Tests.Cases/Generics/DerivedClassWithMethodOfSameNameAsBaseButDifferentNumberOfGenericParametersUnusedBaseWillGetStripped.cs
@@ -0,0 +1,25 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Generics
+{
+	class DerivedClassWithMethodOfSameNameAsBaseButDifferentNumberOfGenericParametersUnusedBaseWillGetStripped
+	{
+		public static void Main(string[] args)
+		{
+			MyDerived obj = new MyDerived();
+			obj.Method<int, int>(1);
+		}
+
+		class MyBase
+		{
+			[Removed]
+			public virtual T Method<T>(T arg1) { return arg1; }
+		}
+
+		class MyDerived : MyBase
+		{
+			[Kept]
+			public virtual T Method<T, K>(T arg1) { return arg1; }
+		}
+	}
+}
