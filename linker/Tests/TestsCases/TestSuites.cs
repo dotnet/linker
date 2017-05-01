9a5a656d30e8bdc267841f12284d06200a7bc6f9
Mike Voorhees <michaelv@unity3d.com>
2017-05-01 10:01:36 -0400

    Split AllTests up into multiple tests

--- linker/Tests/TestsCases/TestSuites.cs
+++ linker/Tests/TestsCases/TestSuites.cs
@@ -8,11 +8,64 @@ namespace Mono.Linker.Tests.TestsCases
 		[TestFixture]
 		public class CommonTests
 		{
-			[TestCaseSource (typeof (TestDatabase), nameof (TestDatabase.AllTests))]
-			public void AllTests (TestCase testCase)
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.BasicTests))]
+			public void BasicTests(TestCase testCase)
 			{
-				var runner = new TestRunner (new ObjectFactory ());
-				runner.Run (testCase);
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.VirtualMethodsTests))]
+			public void VirtualMethodTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.XmlTests))]
+			public void XmlTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.AttributeTests))]
+			public void AttributesTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.GenericsTests))]
+			public void GenericsTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.StaticsTests))]
+			public void StaticsTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.CoreLinkTests))]
+			public void CoreLinkTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.InteropTests))]
+			public void InteropTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			[TestCaseSource(typeof(TestDatabase), nameof(TestDatabase.UngroupedTests))]
+			public void UngroupedTests(TestCase testCase)
+			{
+				Run(testCase);
+			}
+
+			private void Run(TestCase testCase)
+			{
+				var runner = new TestRunner(new ObjectFactory());
+				runner.Run(testCase);
 			}
 		}
 	}
