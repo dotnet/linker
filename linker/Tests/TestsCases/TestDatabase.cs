b25fe4cc16c7d1d59d9172b3b6dfffc36235c3a2
Mike Voorhees <michaelv@unity3d.com>
2017-05-01 10:01:36 -0400

    Split AllTests up into multiple tests

--- linker/Tests/TestsCases/TestDatabase.cs
+++ linker/Tests/TestsCases/TestDatabase.cs
@@ -9,25 +9,94 @@ namespace Mono.Linker.Tests.TestsCases
 {
 	public class TestDatabase
 	{
-		public static IEnumerable<TestCaseData> AllTests ()
+		public static IEnumerable<TestCaseData> XmlTests()
 		{
-			return AllTestCases ();
+			return NUnitCasesByPrefix("LinkXml.");
 		}
 
-		static IEnumerable<TestCaseData> AllTestCases ([CallerFilePath] string thisFile = null)
+		public static IEnumerable<TestCaseData> BasicTests()
 		{
-			var thisDirectory = Path.GetDirectoryName (thisFile);
-			var src_root = Path.GetFullPath (Path.Combine (thisDirectory, "..", "Mono.Linker.Tests.Cases"));
+			return NUnitCasesByPrefix("Basic.");
+		}
 
-			// TODO: Why? We don't need the assembly at all
-			var assembly_path = Path.GetFullPath (Path.Combine (src_root, "bin", "Debug", "Mono.Linker.Tests.Cases.dll"));
+		public static IEnumerable<TestCaseData> VirtualMethodsTests()
+		{
+			return NUnitCasesByPrefix("VirtualMethods.");
+		}
 
-			var testCases = new TestCaseCollector (src_root, assembly_path);
-			foreach (var test in testCases.Collect ().OrderBy (t => t.DisplayName)) {
-				var data = new TestCaseData (test);
-				data.SetName (test.DisplayName);
-				yield return data;
-			}
+		public static IEnumerable<TestCaseData> AttributeTests()
+		{
+			return NUnitCasesByPrefix("Attributes.");
+		}
+
+		public static IEnumerable<TestCaseData> GenericsTests()
+		{
+			return NUnitCasesByPrefix("Generics.");
+		}
+
+		public static IEnumerable<TestCaseData> CoreLinkTests()
+		{
+			return NUnitCasesByPrefix("CoreLink.");
+		}
+
+		public static IEnumerable<TestCaseData> StaticsTests()
+		{
+			return NUnitCasesByPrefix("Statics.");
+		}
+
+		public static IEnumerable<TestCaseData> InteropTests()
+		{
+			return NUnitCasesByPrefix("Interop.");
+		}
+
+		public static IEnumerable<TestCaseData> UngroupedTests()
+		{
+			var allGroupedTestNames = new HashSet<string>(
+				XmlTests()
+					.Concat(BasicTests())
+					.Concat(XmlTests())
+					.Concat(VirtualMethodsTests())
+					.Concat(AttributeTests())
+					.Concat(GenericsTests())
+					.Concat(CoreLinkTests())
+					.Concat(StaticsTests())
+					.Concat(InteropTests())
+					.Select(c => ((TestCase)c.Arguments[0]).FullTypeName));
+
+			return AllCases().Where(c => !allGroupedTestNames.Contains(c.FullTypeName)).Select(c => CreateNUnitTestCase(c, c.DisplayName));
+		}
+
+		static IEnumerable<TestCase> AllCases()
+		{
+			string rootSourceDirectory;
+			string testCaseAssemblyPath;
+			GetDirectoryPaths(out rootSourceDirectory, out testCaseAssemblyPath);
+			return new TestCaseCollector(rootSourceDirectory, testCaseAssemblyPath)
+				.Collect()
+				.OrderBy(c => c.DisplayName)
+				.ToArray();
+		}
+
+		static IEnumerable<TestCaseData> NUnitCasesByPrefix(string testNamePrefix)
+		{
+			return AllCases()
+				.Where(c => c.DisplayName.StartsWith(testNamePrefix))
+				.Select(c => CreateNUnitTestCase(c, c.DisplayName.Substring(testNamePrefix.Length)))
+				.OrderBy(c => c.TestName);
+		}
+
+		static TestCaseData CreateNUnitTestCase(TestCase testCase, string displayName)
+		{
+			var data = new TestCaseData(testCase);
+			data.SetName(displayName);
+			return data;
+		}
+
+		static void GetDirectoryPaths(out string rootSourceDirectory, out string testCaseAssemblyPath, [CallerFilePath] string thisFile = null)
+		{
+			var thisDirectory = Path.GetDirectoryName(thisFile);
+			rootSourceDirectory = Path.GetFullPath(Path.Combine(thisDirectory, "..", "Mono.Linker.Tests.Cases"));
+			testCaseAssemblyPath = Path.GetFullPath(Path.Combine(rootSourceDirectory, "bin", "Debug", "Mono.Linker.Tests.Cases.dll"));
 		}
 	}
 }
