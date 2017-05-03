c040de87bc306cdcd353a71841365eaf3b9c31df
Mike Voorhees <michaelv@unity3d.com>
2017-05-03 12:59:04 -0400

    Bring over more test cases

---
+++ linker/Tests/Mono.Linker.Tests.Cases/VirtualMethods/ClassImplemtingInterfaceMethodsThroughBaseClass5.cs
@@ -0,0 +1,26 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.VirtualMethods {
+	class ClassImplemtingInterfaceMethodsThroughBaseClass5 {
+		public static void Main ()
+		{
+			new A ();
+		}
+
+		interface IFoo {
+			[Removed]
+			void Foo ();
+		}
+
+		class B {
+			[Removed]
+			public void Foo ()
+			{
+			}
+		}
+
+		class A : B, IFoo {
+			//my IFoo.Foo() is actually implemented by B which doesn't know about it.
+		}
+	}
+}
---
+++ linker/Tests/Mono.Linker.Tests.Cases/VirtualMethods/ClassImplemtingInterfaceMethodsThroughBaseClass6.cs
@@ -0,0 +1,36 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.VirtualMethods {
+	[IgnoreTestCase ("This test fails and is ignored for an unknown reason. We should investigate this more.")]
+	class ClassImplemtingInterfaceMethodsThroughBaseClass6 {
+		public static void Main ()
+		{
+			B tmp = new B ();
+			IFoo i = new C ();
+			i.Foo ();
+		}
+
+		interface IFoo {
+			[Kept]
+			void Foo ();
+		}
+
+		class B {
+			[Removed]
+			public void Foo ()
+			{
+			}
+		}
+
+		class A : B, IFoo {
+			//my IFoo.Foo() is actually implemented by B which doesn't know about it.
+		}
+
+		class C : IFoo {
+			[Kept]
+			public void Foo ()
+			{
+			}
+		}
+	}
+}
