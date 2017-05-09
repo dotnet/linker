f10fd65eca44f8c4c2cb85b2026811df06dfa79d
Marek Safar <marek.safar@gmail.com>
2017-05-09 16:02:24 +0200

    Simplify KeptBaseTypeAttribute declaration

--- linker/Tests/Mono.Linker.Tests.Cases.Expectations/Assertions/KeptBaseTypeAttribute.cs
+++ linker/Tests/Mono.Linker.Tests.Cases.Expectations/Assertions/KeptBaseTypeAttribute.cs
@@ -11,19 +11,12 @@ namespace Mono.Linker.Tests.Cases.Expectations.Assertions
 		public KeptBaseTypeAttribute (Type baseType)
 		{
 			BaseType = baseType;
-			GenericParameterNames = null;
 		}
 
-		public KeptBaseTypeAttribute (Type baseType, string arg1Name)
+		public KeptBaseTypeAttribute (Type baseType, params object[] typeArguments)
 		{
 			BaseType = baseType;
-			GenericParameterNames = new [] { arg1Name };
-		}
-
-		public KeptBaseTypeAttribute (Type baseType, string arg1Name, Type arg2Name, Type arg3Name, Type arg4Name)
-		{
-			BaseType = baseType;
-			GenericParameterNames = new object [] { arg1Name, arg2Name, arg3Name, arg4Name };
+			GenericParameterNames = typeArguments;
 		}
 	}
 }
