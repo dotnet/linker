54608dcd8dd14a566b0afc25c4640ebfcc5ef0ce
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-05-26 18:43:22 +0200

    [linker] Provide the inner exception when throwing AssemblyResolutionExceptions to ease debugging.

--- linker/Mono.Linker/LinkContext.cs
+++ linker/Mono.Linker/LinkContext.cs
@@ -163,8 +163,8 @@ namespace Mono.Linker {
 
 				return assembly;
 			}
-			catch {
-				throw new AssemblyResolutionException (reference);
+			catch (Exception e) {
+				throw new AssemblyResolutionException (reference, e);
 			}
 		}
 
