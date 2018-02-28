dotnet/linker

commit f9c0d99fd8630c508f2c801552282b8218ff9d3c

Fix for embedded pdb when copy and no link symbols

linker/Linker/LinkContext.cs
@@ -161,6 +161,8 @@ namespace Mono.Linker {
 			_actions = new Dictionary<string, AssemblyAction> ();
 			_parameters = new Dictionary<string, string> ();
 			_readerParameters = readerParameters;
+			
+			SymbolReaderProvider = new DefaultSymbolReaderProvider (false);
 
 			if (factory == null)
 				throw new ArgumentNullException (nameof (factory));
@@ -225,23 +227,21 @@ namespace Mono.Linker {
 
 		public virtual void SafeReadSymbols (AssemblyDefinition assembly)
 		{
-			if (!_linkSymbols)
-				return;
-
 			if (assembly.MainModule.HasSymbols)
 				return;
 
-			try {
-				if (_symbolReaderProvider != null) {
-					var symbolReader = _symbolReaderProvider.GetSymbolReader (
-						assembly.MainModule,
-						assembly.MainModule.FileName);
+			if (_symbolReaderProvider == null)
+				throw new ArgumentNullException (nameof (_symbolReaderProvider));
+			
+			var symbolReader = _symbolReaderProvider.GetSymbolReader (
+				assembly.MainModule,
+				assembly.MainModule.FileName);
 
-					_annotations.AddSymbolReader (assembly, symbolReader);
-					assembly.MainModule.ReadSymbols (symbolReader);
-				} else
-					assembly.MainModule.ReadSymbols ();
-			} catch {}
+			if (symbolReader == null)
+				return;
+
+			_annotations.AddSymbolReader (assembly, symbolReader);
+			assembly.MainModule.ReadSymbols (symbolReader);
 		}
 
 		public virtual ICollection<AssemblyDefinition> ResolveReferences (AssemblyDefinition assembly)

linker/Linker.Steps/OutputStep.cs
@@ -94,7 +94,12 @@ namespace Mono.Linker.Steps {
 				(module.Attributes & (ModuleAttributes) 0x04) != 0;
 		}
 
-		protected virtual void WriteAssembly (AssemblyDefinition assembly, string directory)
+		protected void WriteAssembly (AssemblyDefinition assembly, string directory)
+		{
+			WriteAssembly (assembly, directory, SaveSymbols (assembly));
+		}
+
+		protected virtual void WriteAssembly (AssemblyDefinition assembly, string directory, WriterParameters writerParameters)
 		{
 			foreach (var module in assembly.Modules) {
 				// Write back pure IL even for R2R assemblies
@@ -105,7 +110,7 @@ namespace Mono.Linker.Steps {
 				}
 			}
 
-			assembly.Write (GetAssemblyFileName (assembly, directory), SaveSymbols (assembly));
+			assembly.Write (GetAssemblyFileName (assembly, directory), writerParameters);
 		}
 
 		void OutputAssembly (AssemblyDefinition assembly)
@@ -201,6 +206,14 @@ namespace Mono.Linker.Steps {
 
 		protected virtual void CopyAssembly (AssemblyDefinition assembly, string directory)
 		{
+			// Special case.  When an assembly has embedded pdbs, link symbols is not enabled, and the assembly's action is copy,
+			// we want to match the behavior of assemblies with the other symbol types and end up with an assembly that does not have symbols.
+			// In order to do that, we can't simply copy files.  We need to write the assembly without symbols
+			if (assembly.MainModule.HasSymbols && !Context.LinkSymbols && assembly.MainModule.SymbolReader is EmbeddedPortablePdbReader) {
+				WriteAssembly (assembly, directory, new WriterParameters ());
+				return;
+			}
+
 			FileInfo fi = GetOriginalAssemblyFileInfo (assembly);
 			string target = Path.GetFullPath (Path.Combine (directory, fi.Name));
 			string source = fi.FullName;

linker/Tests/Mono.Linker.Tests.Cases/Symbols/ReferenceWithEmbeddedPdbCopyAction.cs
@@ -3,7 +3,6 @@ using Mono.Linker.Tests.Cases.Expectations.Metadata;
 using Mono.Linker.Tests.Cases.Symbols.Dependencies;
 
 namespace Mono.Linker.Tests.Cases.Symbols {
-	[IgnoreTestCase ("Will fix in follow on PR.  Fails due to embedded symbols not being removed")]
 	[SetupCompileBefore ("LibraryWithEmbeddedPdbSymbols.dll", new[] { "Dependencies/LibraryWithEmbeddedPdbSymbols.cs" }, additionalArguments: "/debug:embedded", compilerToUse: "csc")]
 	[SetupLinkerLinkSymbols ("false")]
 	[SetupLinkerAction ("copy", "LibraryWithEmbeddedPdbSymbols")]

commit dccb0d2378b4471c3d398a13554526d2f08192c8

Fix a regression introduced in f9c0d99, putting back the try/catch (#268)
`LinkContext.SafeReadSymbols` used to have a try / catch block around the actual symbol reading.

This got removed in f9c0d99 and is now breaking the monthly Xamarin.Android integration.

linker/Linker/LinkContext.cs
@@ -233,16 +233,18 @@ namespace Mono.Linker {
 
 			if (_symbolReaderProvider == null)
 				throw new ArgumentNullException (nameof (_symbolReaderProvider));
-			
-			var symbolReader = _symbolReaderProvider.GetSymbolReader (
-				assembly.MainModule,
-				assembly.MainModule.FileName);
 
-			if (symbolReader == null)
-				return;
+			try {
+				var symbolReader = _symbolReaderProvider.GetSymbolReader (
+					assembly.MainModule,
+					assembly.MainModule.FileName);
 
-			_annotations.AddSymbolReader (assembly, symbolReader);
-			assembly.MainModule.ReadSymbols (symbolReader);
+				if (symbolReader == null)
+					return;
+
+				_annotations.AddSymbolReader (assembly, symbolReader);
+				assembly.MainModule.ReadSymbols (symbolReader);
+			} catch { }
 		}
 
 		public virtual ICollection<AssemblyDefinition> ResolveReferences (AssemblyDefinition assembly)