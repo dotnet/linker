37405affbabd03ea279f017f9e034185dcfa6a91
Marek Safar <marek.safar@gmail.com>
2019-01-08 17:36:43 +0200

    Use ToString to include full name
    Co-Authored-By: kant2002 <kant2002@gmail.com>

--- linker/Linker/Annotations.cs
+++ linker/Linker/Annotations.cs
@@ -87,7 +87,7 @@ namespace Mono.Linker {
 			if (assembly_actions.TryGetValue (assembly, out action))
 				return action;
 
-			throw new InvalidOperationException($"No action for the assembly {assembly.Name.Name} defined");
+			throw new InvalidOperationException($"No action for the assembly {assembly.Name} defined");
 		}
 
 		public MethodAction GetAction (MethodDefinition method)
