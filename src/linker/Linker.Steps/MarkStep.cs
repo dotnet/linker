b510658ca9a03d8d35015f1e1af2d85af1a66218
Michael Voorhees <michaelv@unity3d.com>
2019-02-08 11:45:42 -0500

    Fix a bug with preserve parent
    The test `NeverInstantiatedTypeWithBaseInCopiedAssembly` is the case that would hit an invalid IL bug.
    
    Made other changes to how the marking of virtuals due to a preserved parent works when the type has not been instantiated yet.

--- src/linker/Linker.Steps/MarkStep.cs
+++ src/linker/Linker.Steps/MarkStep.cs
@@ -1014,6 +1014,7 @@ namespace Mono.Linker.Steps {
 				_typesWithInterfaces.Add (type);
 
 			if (type.HasMethods) {
+				MarkMethodsIf (type.Methods, IsVirtualNeededByTypeDueToPreservedScope);
 				if (ShouldMarkTypeStaticConstructor (type))
 					MarkStaticConstructor (type);
 
@@ -1325,7 +1326,38 @@ namespace Mono.Linker.Steps {
 				MarkType (constraint);
 		}
 
-		bool IsVirtualAndHasPreservedParent (MethodDefinition method)
+		bool IsVirtualNeededByTypeDueToPreservedScope (MethodDefinition method)
+		{
+			if (!method.IsVirtual)
+				return false;
+
+			var base_list = Annotations.GetBaseMethods (method);
+			if (base_list == null)
+				return false;
+
+			foreach (MethodDefinition @base in base_list) {
+				// Just because the type is marked does not mean we need interface methods.
+				// if the type is never instantiated, interfaces will be removed
+				if (@base.DeclaringType.IsInterface)
+					continue;
+				
+				// If the type is marked, we need to keep overrides of abstract members defined in assemblies
+				// that are copied.  However, if the base method is virtual, then we don't need to keep the override
+				// until the type could be instantiated
+				if (!@base.IsAbstract)
+					continue;
+
+				if (IgnoreScope (@base.DeclaringType.Scope))
+					return true;
+
+				if (IsVirtualNeededByTypeDueToPreservedScope (@base))
+					return true;
+			}
+
+			return false;
+		}
+		
+		bool IsVirtualNeededByInstantiatedTypeDueToPreservedScope (MethodDefinition method)
 		{
 			if (!method.IsVirtual)
 				return false;
@@ -1338,7 +1370,7 @@ namespace Mono.Linker.Steps {
 				if (IgnoreScope (@base.DeclaringType.Scope))
 					return true;
 
-				if (IsVirtualAndHasPreservedParent (@base))
+				if (IsVirtualNeededByTypeDueToPreservedScope (@base))
 					return true;
 			}
 
@@ -1818,7 +1850,7 @@ namespace Mono.Linker.Steps {
 			foreach (var method in type.Methods) {
 				if (method.IsFinalizer ())
 					MarkMethod (method);
-				else if (IsVirtualAndHasPreservedParent (method))
+				else if (IsVirtualNeededByInstantiatedTypeDueToPreservedScope (method))
 					MarkMethod (method);
 			}
 
