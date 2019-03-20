ff773cfd7d5bb22a9e7512ffdf2b65def125faee
Sven Boemer <sbomer@gmail.com>
2019-03-20 12:28:51 -0700

    Fix testcase paths when running tests with arcade
    This works around problems where deterministic builds remove full file
    paths from the source info, causing [CallerFilePath] to give back
    paths like "/_/test/Mono.Linker.Tests/TestCases/TestDatabase.cs".
    
    Instead, we now compute the test directories from the working
    directory, which as far as I can tell is consistently set to the
    "<config>/<tfm>" directory of the test project binary folder.
    
    Building with arcade changes the binary output layout, so we need a
    special case for this.

--- test/Mono.Linker.Tests/TestCases/TestDatabase.cs
+++ test/Mono.Linker.Tests/TestCases/TestDatabase.cs
@@ -162,8 +162,7 @@ namespace Mono.Linker.Tests.TestCases
 
 		static void GetDirectoryPaths(out string rootSourceDirectory, out string testCaseAssemblyPath, [CallerFilePath] string thisFile = null)
 		{
-			var thisDirectory = Path.GetDirectoryName(thisFile);
-			rootSourceDirectory = Path.GetFullPath(Path.Combine(thisDirectory, "..", "..", "Mono.Linker.Tests.Cases"));
+
 #if ILLINK
 #if DEBUG
 			var configDirectoryName = "illink_Debug";
@@ -183,7 +182,27 @@ namespace Mono.Linker.Tests.TestCases
 #else
 			var tfm = "";
 #endif
-			testCaseAssemblyPath = Path.GetFullPath(Path.Combine(rootSourceDirectory, "bin", configDirectoryName, tfm, "Mono.Linker.Tests.Cases.dll"));
+
+#if ILLINK
+			// Deterministic builds sanitize source paths, so CallerFilePathAttribute gives an incorrect path.
+			// Instead, get the testcase dll based on the working directory of the test runner.
+#if ARCADE
+			// working directory is artifacts/bin/Mono.Linker.Tests/<config>/<tfm>
+			var artifactsBinDir = Path.Combine (Directory.GetCurrentDirectory (), "..", "..", "..");
+			rootSourceDirectory = Path.GetFullPath (Path.Combine (artifactsBinDir, "..", "..", "test", "Mono.Linker.Tests.Cases"));
+			testCaseAssemblyPath = Path.GetFullPath (Path.Combine (artifactsBinDir, "Mono.Linker.Tests.Cases", configDirectoryName, tfm, "Mono.Linker.Tests.Cases.dll"));
+#else
+			// working directory is test/Mono.Linker.Tests/bin/<config>/<tfm>
+			var testDir = Path.Combine (Directory.GetCurrentDirectory (), "..", "..", "..", "..");
+			rootSourceDirectory = Path.GetFullPath (Path.Combine (testDir, "Mono.Linker.Tests.Cases"));
+			testCaseAssemblyPath = Path.GetFullPath (Path.Combine (rootSourceDirectory, "bin", configDirectoryName, tfm, "Mono.Linker.Tests.Cases.dll"));
+#endif // ARCADE
+
+#else
+			var thisDirectory = Path.GetDirectoryName (thisFile);
+			rootSourceDirectory = Path.GetFullPath (Path.Combine (thisDirectory, "..", "..", "Mono.Linker.Tests.Cases"));
+			testCaseAssemblyPath = Path.GetFullPath (Path.Combine (rootSourceDirectory, "bin", configDirectoryName, tfm, "Mono.Linker.Tests.Cases.dll"));
+#endif // ILLINK
 		}
 	}
 }
