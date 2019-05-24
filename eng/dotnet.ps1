53ff6be481527f0beda69655fb3e50adb325aaa2
Sven Boemer <sbomer@gmail.com>
2019-05-24 16:37:36 -0700

    Update SDK

--- eng/dotnet.ps1
+++ eng/dotnet.ps1
@@ -1,5 +1,6 @@
 . $PSScriptRoot/common/tools.ps1
 
+$useInstalledDotNetCli = $false
 $dotnetRoot = InitializeDotNetCli -install:$true
 Join-Path $dotnetRoot "dotnet.exe"
 $exitCode = Exec-Process (Join-Path $dotnetRoot "dotnet.exe") $args
--- eng/dotnet.sh
+++ eng/dotnet.sh
@@ -4,6 +4,7 @@ source="${BASH_SOURCE[0]}"
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 . "$scriptroot/common/tools.sh"
 
+use_installed_dotnet_cli=false
 InitializeDotNetCli true
 echo "$_InitializeDotNetCli/dotnet" "$@"
 "$_InitializeDotNetCli/dotnet" "$@"
