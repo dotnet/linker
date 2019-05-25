89676b420b11100df5f2723f82001bd46b1c9e22
Sven Boemer <sbomer@gmail.com>
2019-05-24 17:59:55 -0700

    Add CheckProjects test

---
+++ test/ILLink.Tasks.Tests/CheckProjects.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace ILLink.Tests
+{
+	public class CheckProjects : IntegrationTestBase, IClassFixture<ConsoleFixture>
+	{
+		public CheckProjects(ConsoleFixture fixture, ITestOutputHelper helper) : base(fixture, helper) {}
+
+		[Fact]
+		public void CheckTargetIncludes()
+		{
+			var projectRoot = Path.GetDirectoryName(Fixture.csproj);
+			var ret = CommandHelper.Dotnet("msbuild /t:CheckTargetIncludes", projectRoot);
+			Assert.True(ret.ExitCode == 0);
+			Assert.Contains("test logic targets were included", ret.StdOut);
+		}
+
+		[Fact]
+		public void CheckTasksPath()
+		{
+			var projectRoot = Path.GetDirectoryName(Fixture.csproj);
+			var ret = CommandHelper.Dotnet("msbuild /t:CheckTasksPath", projectRoot);
+			Assert.True(ret.ExitCode == 0);
+			Assert.Contains($"_ILLinkTasksDirectoryRoot: {TestContext.TasksDirectoryRoot}", ret.StdOut);
+		}
+	}
+}
