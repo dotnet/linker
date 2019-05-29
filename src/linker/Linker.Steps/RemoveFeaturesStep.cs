f92fc21735ae37a55cf55c30132c884623100ee8
Martin Baulig <mabaul@microsoft.com>
2019-05-29 11:36:37 -0400

    Add custom logic for reflection-emit to RemoveFeaturesStep.
    When `--exclude-feature sre` is provided, then we rewrite the
    `System.RuntimeType.MakeTypeBuilderInstantiation` to throw.

--- src/linker/Linker.Steps/RemoveFeaturesStep.cs
+++ src/linker/Linker.Steps/RemoveFeaturesStep.cs
@@ -12,6 +12,7 @@ namespace Mono.Linker.Steps
 
 		public bool FeatureCOM { get; set; }
 		public bool FeatureETW { get; set; }
+		public bool FeatureSRE { get; set; }
 
 		//
 		// Manually overrides System.Globalization.Invariant mode
@@ -55,6 +56,15 @@ namespace Mono.Linker.Steps
 					ExcludeEventSource (type);
 				else if (BCL.EventTracingForWindows.IsEventSourceImplementation (type))
 					ExcludeEventSourceImplementation (type);
+			}
+
+			if (FeatureSRE) {
+				if (type.FullName == "System.RuntimeType") {
+					foreach (var method in type.Methods) {
+						if (method.Name == "MakeTypeBuilderInstantiation")
+							Annotations.SetAction (method, MethodAction.ConvertToThrow);
+					}
+				}
 			}
 
 			if (RemoveCustomAttributes (type)) {
