af092088898ae75b057c5367778421340cc1d30e
Marek Safar <marek.safar@gmail.com>
2019-09-12 15:54:46 +0200

    Tests update

--- test/Mono.Linker.Tests.Cases/CommandLine/Mvid/RetainMvid.cs
+++ test/Mono.Linker.Tests.Cases/CommandLine/Mvid/RetainMvid.cs
@@ -2,7 +2,7 @@ using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.CommandLine.Mvid {
-	[SetupLinkerArgument ("--new-mvid", "false")]
+	[SetupLinkerArgument ("--deterministic", "true")]
 	public class RetainMvid
 	{
 		public static void Main ()
