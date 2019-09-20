c7fb6dec6bb67d66a663877b1a06a3cbb298ab41
Marek Safar <marek.safar@gmail.com>
2019-09-20 17:47:53 +0200

    Switch to C# 8.0 for tests

--- test/Mono.Linker.Tests.Cases/Attributes/OnlyKeepUsed/NullableOnConstraints.cs
+++ test/Mono.Linker.Tests.Cases/Attributes/OnlyKeepUsed/NullableOnConstraints.cs
@@ -1,3 +1,4 @@
+#if !ILLINK
 #nullable enable
 
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
@@ -44,3 +45,4 @@ namespace Mono.Linker.Tests.Cases.Attributes.OnlyKeepUsed {
 	{
 	}	
 }
+#endif
