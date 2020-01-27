d5d917bec4d116f0c45dc774fd1d0214afedf5b6
vitek-karas <vitek.karas@microsoft.com>
2020-01-27 12:27:51 -0800

    PR feedback

--- src/linker/Linker.Steps/MarkStep.cs
+++ src/linker/Linker.Steps/MarkStep.cs
@@ -2378,11 +2378,21 @@ namespace Mono.Linker.Steps {
 			}
 		}
 
+		/// <summary>
+		/// Helper struct to pass around context information about reflection pattern
+		/// as a single parameter (and have a way to extend this in the future if we need to easily).
+		/// Also implements a simple validation mechanism to check that the code does report patter recognition
+		/// results for all methods it works on.
+		/// The promise of the pattern recorder is that for a given reflection method, it will either not talk
+		/// about it ever, or it will always report recognized/unrecognized.
+		/// </summary>
 		struct ReflectionPatternContext : IDisposable
 		{
 			readonly LinkContext _context;
+#if DEBUG
 			bool _patternAnalysisAttempted;
 			bool _patternReported;
+#endif
 
 			public MethodDefinition MethodCalling { get; private set; }
 			public MethodDefinition MethodCalled { get; private set; }
@@ -2395,13 +2405,18 @@ namespace Mono.Linker.Steps {
 				MethodCalled = methodCalled;
 				InstructionIndex = instructionIndex;
 
+#if DEBUG
 				_patternAnalysisAttempted = false;
 				_patternReported = false;
+#endif
 			}
 
+			[Conditional("DEBUG")]
 			public void AnalyzingPattern ()
 			{
+#if DEBUG
 				_patternAnalysisAttempted = true;
+#endif
 			}
 
 			public void RecordRecognizedPattern<T> (T accessedItem, Action mark)
@@ -2411,7 +2426,9 @@ namespace Mono.Linker.Steps {
 
 				_context.Tracer.Push ($"Reflection-{accessedItem}");
 				try {
+#if DEBUG
 					_patternReported = true;
+#endif
 					mark ();
 					_context.ReflectionPatternRecorder.RecognizedReflectionAccessPattern (MethodCalling, MethodCalled, accessedItem);
 				} finally {
@@ -2423,15 +2440,15 @@ namespace Mono.Linker.Steps {
 			{
 				Debug.Assert (_patternAnalysisAttempted, "To correctly report all patterns, when starting to analyze a pattern the AnalyzingPattern must be called first.");
 
+#if DEBUG
 				_patternReported = true;
+#endif
 				_context.ReflectionPatternRecorder.UnrecognizedReflectionAccessPattern (MethodCalling, MethodCalled, message);
 			}
 
 			public void Dispose ()
 			{
-				if (_patternAnalysisAttempted && !_patternReported) {
-					Debug.Fail ("A reflection pattern was analyzed, but no result was reported.");
-				}
+				Debug.Assert(!_patternAnalysisAttempted || _patternReported, "A reflection pattern was analyzed, but no result was reported.");
 			}
 		}
 
