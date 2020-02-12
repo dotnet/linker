6ad20f2fe2ba5e82a8fa729aae3cb8bec4ebb1f8
Marek Safar <marek.safar@gmail.com>
2020-02-12 10:40:02 +0100

    Undo tests changes

--- test/Mono.Linker.Tests.Cases/CommandLine/Mvid/NewMvidWorks.cs
+++ test/Mono.Linker.Tests.Cases/CommandLine/Mvid/NewMvidWorks.cs
@@ -2,7 +2,7 @@ using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.CommandLine.Mvid {
-	[SetupLinkerArgument ("--deterministic", "false")]
+	[SetupLinkerArgument ("--new-mvid", "true")]
 	public class NewMvidWorks {
 		public static void Main ()
 		{
--- test/Mono.Linker.Tests.Cases/CommandLine/Mvid/RetainMvid.cs
+++ test/Mono.Linker.Tests.Cases/CommandLine/Mvid/RetainMvid.cs
@@ -2,7 +2,7 @@ using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.CommandLine.Mvid {
-	[SetupLinkerArgument ("--deterministic", "true")]
+	[SetupLinkerArgument ("--new-mvid", "false")]
 	public class RetainMvid
 	{
 		public static void Main ()
