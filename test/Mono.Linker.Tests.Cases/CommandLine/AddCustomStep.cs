cd9dcb8e4bdb17bf7f9f7472506921e484e27715
Vitek Karas <vitek.karas@microsoft.com>
2020-03-10 03:51:00 -0700

    Update test/Mono.Linker.Tests.Cases/CommandLine/AddCustomStep.cs
    Co-Authored-By: Marek Safar <marek.safar@gmail.com>

--- test/Mono.Linker.Tests.Cases/CommandLine/AddCustomStep.cs
+++ test/Mono.Linker.Tests.Cases/CommandLine/AddCustomStep.cs
@@ -4,7 +4,7 @@ using Mono.Linker.Tests.Cases.Expectations.Metadata;
 namespace Mono.Linker.Tests.Cases.CommandLine
 {
 
-#if NET471
+#if !NETCOREAPP
 	[IgnoreTestCase ("Can be enabled once MonoBuild produces a dll from which we can grab the types in the Mono.Linker namespace.")]
 #else
 	[SetupCompileBefore ("CustomStep.dll", new [] { "Dependencies/CustomStepDummy.cs" }, new [] { "illink.dll" })]
