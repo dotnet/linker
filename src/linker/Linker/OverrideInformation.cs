7af9f5a43820ff4fc97651d5cbe000c399c74310
Marek Safar <marek.safar@gmail.com>
2020-03-27 14:00:24 +0100

    Add core externally used apis to ref assembly

--- src/linker/Linker/OverrideInformation.cs
+++ src/linker/Linker/OverrideInformation.cs
@@ -4,16 +4,16 @@ using Mono.Cecil;
 namespace Mono.Linker {
 	[DebuggerDisplay ("{Override}")]
 	public class OverrideInformation {
-		public readonly MethodDefinition Base;
-		public readonly MethodDefinition Override;
-		public readonly InterfaceImplementation MatchingInterfaceImplementation;
-
 		public OverrideInformation (MethodDefinition @base, MethodDefinition @override, InterfaceImplementation matchingInterfaceImplementation = null)
 		{
 			Base = @base;
 			Override = @override;
 			MatchingInterfaceImplementation = matchingInterfaceImplementation;
 		}
+
+		public MethodDefinition Base { get; private set; }
+		public MethodDefinition Override { get; private set; }
+		public InterfaceImplementation MatchingInterfaceImplementation { get; private set; }
 
 		public bool IsOverrideOfInterfaceMember
 		{
