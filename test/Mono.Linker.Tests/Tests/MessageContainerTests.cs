d706d8fdeec1c161e0755ccdfa0a1b090d235d50
Marek Safar <marek.safar@gmail.com>
2020-03-30 22:29:03 +0200

    Update MessageContainerTests.cs

--- test/Mono.Linker.Tests/Tests/MessageContainerTests.cs
+++ test/Mono.Linker.Tests/Tests/MessageContainerTests.cs
@@ -4,18 +4,18 @@ namespace Mono.Linker.Tests
 {
 	[TestFixture]
 	public class MessageContainerTests
-    {
+	{
 		[Test]
 		public void MSBuildFormat ()
 		{
-            var msg = MessageContainer.CreateErrorMessage ("text", 0);
-            Assert.AreEqual ("illinker: error IL0000: text", msg.ToMSBuildString ());
+			var msg = MessageContainer.CreateErrorMessage ("text", 0);
+			Assert.AreEqual ("illinker: error IL0000: text", msg.ToMSBuildString ());
 
 			msg = MessageContainer.CreateWarningMessage ("message", 2001, origin: new MessageOrigin("logtest", 1, 1));
-            Assert.AreEqual ("logtest(1,1): warning IL2001: message", msg.ToMSBuildString ());
+			Assert.AreEqual ("logtest(1,1): warning IL2001: message", msg.ToMSBuildString ());
 
 			msg = MessageContainer.CreateInfoMessage ("log test");
-            Assert.AreEqual ("illinker: log test", msg.ToMSBuildString ());
+			Assert.AreEqual ("illinker: log test", msg.ToMSBuildString ());
 		}
 	}
 }
