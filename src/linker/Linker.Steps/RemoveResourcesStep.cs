2623fa958a14bb5bc846770897d813111310bdb2
William Barbosa <heytherewill@gmail.com>
2020-03-31 18:02:25 -0300

    Use Tabs

--- src/linker/Linker.Steps/RemoveResourcesStep.cs
+++ src/linker/Linker.Steps/RemoveResourcesStep.cs
@@ -12,12 +12,12 @@ namespace Mono.Linker.Steps
 			RemoveFSharpCompilationResources (assembly);
 		}
 
-        private void RemoveFSharpCompilationResources(AssemblyDefinition assembly)
-        {
-            var resourcesInAssembly = assembly.MainModule.Resources.Select(r => r.Name);
-            foreach (var resource in resourcesInAssembly.Where(IsFSharpCompilationResource)) {
-                Annotations.AddResourceToRemove(assembly, resource);
-            }
+		private void RemoveFSharpCompilationResources (AssemblyDefinition assembly)
+		{
+			var resourcesInAssembly = assembly.MainModule.Resources.Select (r => r.Name);
+			foreach (var resource in resourcesInAssembly.Where (IsFSharpCompilationResource)) {
+				Annotations.AddResourceToRemove (assembly, resource);
+			}
 
 			static bool IsFSharpCompilationResource (string resourceName)
 				=> resourceName.StartsWith ("FSharpSignatureData", StringComparison.Ordinal)
