0a3e89d6b2b19b982deb9b9b85161ff2acf48c4d
Sven Boemer <sbomer@gmail.com>
2020-04-10 00:05:06 +0000

    Add dataflow test with backwards edge

---
+++ test/Mono.Linker.Tests.Cases/DataFlow/LocalDataflow.cs
@@ -0,0 +1,54 @@
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using System.Runtime.CompilerServices;
+
+namespace Mono.Linker.Tests.Cases.DataFlow
+{
+	// Note: this test's goal is to validate that the product correctly reports unrecognized patterns
+	//   - so the main validation is done by the UnrecognizedReflectionAccessPattern attributes.
+	[SkipKeptItemsValidation]
+	public class LocalDataflow
+	{
+		public static void Main ()
+		{
+			TestBackwardsEdge ();
+		}
+
+		[RecognizedReflectionAccessPattern]
+		public static void TestBackwardsEdge ()
+		{
+			string str = GetWithMethods ();
+			string prev = null;
+			for (int i = 0; i < 5; i++) {
+				prev = str; // dataflow will only consider the first reaching definition of "str" above
+				str = GetWithFields (); // dataflow will merge values to track both possible annotation kinds
+			}
+
+			// RequireMethods (str); // this would produce a warning for the value that comes from GetWithFields, as expected
+			RequireMethods (prev); // this produces no warning, even though "prev" will have the value from GetWithFields!
+		}
+
+		public static void RequireMethods (
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.Methods)]
+			string s)
+		{
+
+		}
+
+		public static void RequireFields (
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.Fields)]
+			string s)
+		{
+
+		}
+
+		[return: DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.Methods)]
+		public static string GetWithMethods () {
+			return null;
+		}
+
+		[return: DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.Fields)]
+		public static string GetWithFields () {
+			return null;
+		}
+	}
+}
