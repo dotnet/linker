b121982c6e50dacf93fd13ff70b00b7a372fcb1b
vitek-karas <vitek.karas@microsoft.com>
2020-04-17 00:40:08 -0700

    Deduplicate ArrayBuilder helper class

---
+++ src/linker/Linker.Dataflow/ArrayBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mono.Linker.Dataflow
+{
+	struct ArrayBuilder<T>
+	{
+		private List<T> _list;
+
+		public void Add (T value) => (_list ??= new List<T> ()).Add (value);
+
+		public bool Any (Predicate<T> callback) => _list == null ? false : _list.Exists (callback);
+
+		public T [] ToArray () => _list?.ToArray ();
+
+		public int Count => _list?.Count ?? 0;
+	}
+}
