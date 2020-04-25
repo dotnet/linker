dotnet/linker

commit f8d216d8611265af051163b07bccc6fdb83f3571

Add documented way to pass data to custom steps (#1128)
* Add documented way to pass data to custom steps

* Update src/linker/README.md

Co-Authored-By: Vitek Karas <vitek.karas@microsoft.com>

* Update src/linker/Linker.Steps/ClearInitLocals.cs

Co-Authored-By: Vitek Karas <vitek.karas@microsoft.com>

* Update src/linker/Linker.Steps/ClearInitLocals.cs

Co-Authored-By: Vitek Karas <vitek.karas@microsoft.com>

Co-authored-by: Vitek Karas <vitek.karas@microsoft.com>

src/ILLink.Tasks/LinkTask.cs
@@ -324,9 +324,8 @@ namespace ILLink.Tasks
 
 			if (clearInitLocals) {
 				args.AppendLine ("--enable-opt clearinitlocals");
-				if ((ClearInitLocalsAssemblies != null) && (ClearInitLocalsAssemblies.Length > 0)) {
-					args.Append ("-m ClearInitLocalsAssemblies ");
-					args.AppendLine (ClearInitLocalsAssemblies);
+				if (ClearInitLocalsAssemblies?.Length > 0) {
+					args.AppendFormat ($"--custom-data ClearInitLocalsAssemblies={ClearInitLocalsAssemblies}");
 				}
 			}
 

src/linker/Linker.Steps/ClearInitLocals.cs
@@ -10,10 +10,8 @@ namespace Mono.Linker.Steps
 
 		protected override void Process ()
 		{
-			string parameterName = "ClearInitLocalsAssemblies";
 
-			if (Context.HasParameter (parameterName)) {
-				string parameter = Context.GetParameter (parameterName);
+			if (Context.TryGetCustomData ("ClearInitLocalsAssemblies", out string parameter)) {
 				_assemblies = new HashSet<string> (parameter.Split(','), StringComparer.OrdinalIgnoreCase);
 			}
 		}

src/linker/Linker/Driver.cs
@@ -285,6 +285,22 @@ namespace Mono.Linker {
 
 						continue;
 
+					case "--custom-data":
+						if (arguments.Count < 1) {
+							ErrorMissingArgument (token);
+							return -1;
+						}
+
+						var arg = arguments.Dequeue ();
+						string [] values = arg.Split ('=');
+						if (values?.Length != 2) {
+							Console.WriteLine ($"Value used with '--custom-data' has to be in the KEY=VALUE format");
+							return -1;
+						}
+
+						context.SetCustomData (values [0], values [1]);
+						continue;
+
 					case "--keep-facades":
 						if (!GetBoolParam (token, l => context.KeepTypeForwarderOnlyAssemblies = l))
 							return -1;
@@ -460,14 +476,6 @@ namespace Mono.Linker {
 
 						continue;
 #endif
-					case "m":
-						if (arguments.Count < 2) {
-							ErrorMissingArgument (token);
-							return -1;
-						}
-
-						context.SetParameter (arguments.Dequeue (), arguments.Dequeue ());
-						continue;
 					case "b":
 						if (!GetBoolParam (token, l => context.LinkSymbols = l))
 							return -1;
@@ -926,6 +934,7 @@ namespace Mono.Linker {
 			Console.WriteLine ("                            TYPE,PATH_TO_ASSEMBLY: Add user defined type as last step to the pipeline");
 			Console.WriteLine ("                            -NAME:TYPE,PATH_TO_ASSEMBLY: Inserts step type before existing step with name");
 			Console.WriteLine ("                            +NAME:TYPE,PATH_TO_ASSEMBLY: Add step type after existing step");
+			Console.WriteLine ("  --custom-data KEY=VALUE   Populates context data set with user specified key-value pair");
 			Console.WriteLine ("  --ignore-descriptors      Skips reading embedded descriptors (short -z). Defaults to false");
 			Console.WriteLine ("  --keep-facades            Keep assemblies with type-forwarders (short -t). Defaults to false");
 			Console.WriteLine ("  --skip-unresolved         Ignore unresolved types, methods, and assemblies. Defaults to false");

src/linker/Linker/LinkContext.cs
@@ -188,7 +188,7 @@ namespace Mono.Linker {
 			_resolver = resolver;
 			_resolver.Context = this;
 			_actions = new Dictionary<string, AssemblyAction> ();
-			_parameters = new Dictionary<string, string> ();
+			_parameters = new Dictionary<string, string> (StringComparer.Ordinal);
 			_readerParameters = readerParameters;
 			
 			SymbolReaderProvider = new DefaultSymbolReaderProvider (false);
@@ -418,20 +418,19 @@ namespace Mono.Linker {
 			return null;
 		}
 
-		public void SetParameter (string key, string value)
+		public void SetCustomData (string key, string value)
 		{
 			_parameters [key] = value;
 		}
 
-		public bool HasParameter (string key)
+		public bool HasCustomData (string key)
 		{
 			return _parameters.ContainsKey (key);
 		}
 
-		public string GetParameter (string key)
+		public bool TryGetCustomData (string key, out string value)
 		{
-			_parameters.TryGetValue (key, out string val);
-			return val;
+			return _parameters.TryGetValue (key, out value);
 		}
 
 		public void Dispose ()

src/linker/ref/Linker/LinkContext.cs
@@ -15,5 +15,8 @@ namespace Mono.Linker {
 		public AssemblyDefinition GetLoadedAssembly (string name) { throw null; }
 
 		public void LogMessage (MessageContainer message) { throw null; }
+
+		public bool HasCustomData (string key) { throw null; }
+		public bool TryGetCustomData (string key, out string value) { throw null; }
 	}
 }

test/ILLink.Tasks.Tests/ILLink.Tasks.Tests.cs
@@ -298,7 +298,8 @@ namespace ILLink.Tasks.Tests
 			};
 			using (var driver = task.CreateDriver ()) {
 				var expectedAssemblies = assemblyNames.Split (',');
-				var actualAssemblies = driver.Context.GetParameter ("ClearInitLocalsAssemblies").Split (',');
+				Assert.True (driver.Context.TryGetCustomData ("ClearInitLocalsAssemblies", out var value));
+				var actualAssemblies = value.Split (',');
 				Assert.Equal (expectedAssemblies.OrderBy (a => a), actualAssemblies.OrderBy (a => a));
 			}
 		}

test/Mono.Linker.Tests.Cases/CommandLine/CustomStepData.cs
@@ -1,0 +1,22 @@
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.CommandLine
+{
+
+#if !NETCOREAPP
+	[IgnoreTestCase ("Can be enabled once MonoBuild produces a dll from which we can grab the types in the Mono.Linker namespace.")]
+#else
+	[SetupCompileBefore ("CustomStep.dll", new [] { "Dependencies/CustomStepUser.cs" }, new [] { "illink.dll" })]
+#endif
+	[SetupLinkerArgument ("--custom-step", "CustomStep.CustomStepUser,CustomStep.dll")]
+	[SetupLinkerArgument ("--custom-data", "NewKey=UserValue")]
+
+	[LogContains ("Custom step added with custom data of UserValue")]
+	public class CustomStepData
+	{
+		public static void Main ()
+		{
+		}
+	}
+}

test/Mono.Linker.Tests.Cases/CommandLine/Dependencies/CustomStepUser.cs
@@ -1,0 +1,15 @@
+using System;
+using Mono.Linker;
+using Mono.Linker.Steps;
+
+namespace CustomStep
+{
+	public class CustomStepUser : IStep
+	{
+		public void Process(LinkContext context)
+		{
+			if (context.TryGetCustomData ("NewKey", out var value))
+				context.LogMessage ("Custom step added with custom data of " + value);
+		}
+	}
+}

commit b3a0822d30eafaac225f4a894b902a4aece64cc6

Fixes regression caused by missing space after `--custom-data` argument
Introduced in f8d216d8611265af051163b07bccc6fdb83f3571

src/ILLink.Tasks/LinkTask.cs
@@ -331,7 +331,7 @@ namespace ILLink.Tasks
 			if (clearInitLocals) {
 				args.AppendLine ("--enable-opt clearinitlocals");
 				if (ClearInitLocalsAssemblies?.Length > 0) {
-					args.AppendFormat ($"--custom-data ClearInitLocalsAssemblies={ClearInitLocalsAssemblies}");
+					args.AppendFormat ($"--custom-data ClearInitLocalsAssemblies={ClearInitLocalsAssemblies} ");
 				}
 			}
 