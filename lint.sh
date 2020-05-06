dfd3e1a10695c6f108e47eeb455dabd503c815cf
vitek-karas <vitek.karas@microsoft.com>
2020-05-06 11:42:59 -0700

    PR feedback changes

--- lint.sh
+++ lint.sh
@@ -13,4 +13,4 @@ while [[ -h $source ]]; do
 done
 
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-"$scriptroot/eng/common/build.sh" --projects "$scriptroot/illink.sln" --build --restore $@
+"$scriptroot/eng/dotnet.sh" tool run dotnet-format --verbosity diagnostic -f . --exclude src/analyzer,src/tuner,external $@
