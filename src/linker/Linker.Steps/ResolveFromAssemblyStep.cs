8aeb2545d740c534d5690460ce6bf6c02d1c1ef7
Mateo Torres Ruiz <matorre@microsoft.com>
2020-05-14 14:59:47 -0700

    Remove old LogMessage overloads and MessageImportance

--- src/linker/Linker.Steps/ResolveFromAssemblyStep.cs
+++ src/linker/Linker.Steps/ResolveFromAssemblyStep.cs
@@ -122,9 +122,9 @@ namespace Mono.Linker.Steps
 						//
 						// Both cases are bugs not on our end but we still want to link all assemblies
 						// especially when such types cannot be used anyway
-						//
-						context.LogMessage ($"Cannot find declaration of exported type '{exported}' from the assembly '{assembly}'");
-
+						//						
+						context.LogMessage (MessageContainer.CreateInfoMessage ($"Cannot find declaration of exported type '{exported}' from the assembly '{assembly}'"));
+						
 						continue;
 					}
 
