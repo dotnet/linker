d0149687e7d28becf89bb4bca5c153d4d80da3b5
Mateo Torres Ruiz <matorre@microsoft.com>
2020-05-29 13:03:12 -0700

    Don't log warnings and errors by default yet

--- src/linker/Linker/LinkContext.cs
+++ src/linker/Linker/LinkContext.cs
@@ -468,12 +468,8 @@ namespace Mono.Linker
 
 		public void LogMessage (MessageContainer message)
 		{
-			if ((message.Category == MessageCategory.Info ||
-				message.Category == MessageCategory.Diagnostic) &&
-				!LogMessages)
-				return;
-
-			Logger?.LogMessage (message);
+			if (LogMessages)
+				Logger?.LogMessage (message);
 		}
 
 		public void LogMessage (string message, bool isDiagnostic = false)
@@ -500,6 +496,9 @@ namespace Mono.Linker
 		/// <param name="subcategory">Optionally, further categorize this warning</param>
 		public void LogWarning (string text, int code, MessageOrigin origin, string subcategory = MessageSubCategory.None)
 		{
+			if (!LogMessages)
+				return;
+
 			var warning = MessageContainer.CreateWarningMessage (this, text, code, origin, subcategory);
 			Logger?.LogMessage (warning);
 		}
@@ -540,6 +539,9 @@ namespace Mono.Linker
 		/// <returns>New MessageContainer of 'Error' category</returns>
 		public void LogError (string text, int code, string subcategory = MessageSubCategory.None, MessageOrigin? origin = null)
 		{
+			if (!LogMessages)
+				return;
+
 			var error = MessageContainer.CreateErrorMessage (text, code, subcategory, origin);
 			Logger?.LogMessage (error);
 		}
