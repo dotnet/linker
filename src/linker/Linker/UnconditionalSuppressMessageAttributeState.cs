a112c3e90360cc7af8fa175ab9cbb54cf6726406
Mateo Torres Ruiz <matorre@microsoft.com>
2020-05-29 15:59:15 -0700

    Add module level suppressions

--- src/linker/Linker/UnconditionalSuppressMessageAttributeState.cs
+++ src/linker/Linker/UnconditionalSuppressMessageAttributeState.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
 using Mono.Cecil;
 
 namespace Mono.Linker
@@ -7,6 +9,7 @@ namespace Mono.Linker
 	{
 		private readonly LinkContext _context;
 		private readonly Dictionary<ICustomAttributeProvider, Dictionary<int, SuppressMessageInfo>> _localSuppressions;
+		private GlobalSuppressions _globalSuppressions;
 
 		private bool HasLocalSuppressions {
 			get {
@@ -41,8 +44,15 @@ namespace Mono.Linker
 
 		public bool IsSuppressed (int id, MessageOrigin warningOrigin, out SuppressMessageInfo info)
 		{
+			if (IsGloballySuppressed (id, warningOrigin, out info))
+				return true;
+
 			if (HasLocalSuppressions && warningOrigin.MemberDefinition != null) {
 				IMemberDefinition memberDefinition = warningOrigin.MemberDefinition;
+				// Check if it's suppressed by a module level suppression.
+				if (IsLocallySuppressed (id, memberDefinition.DeclaringType.Module, out info))
+					return true;
+
 				while (memberDefinition != null) {
 					if (IsLocallySuppressed (id, memberDefinition, out info))
 						return true;
@@ -103,6 +113,71 @@ namespace Mono.Linker
 			info = default;
 			return _localSuppressions.TryGetValue (provider, out suppressions) &&
 				suppressions.TryGetValue (id, out info);
+		}
+
+		private bool IsGloballySuppressed (int id, MessageOrigin origin, out SuppressMessageInfo info)
+		{
+			DecodeGlobalSuppressMessageAttributes ();
+			// TODO
+			info = default;
+			return false;
+		}
+
+		private void DecodeGlobalSuppressMessageAttributes ()
+		{
+			if (_globalSuppressions == null) {
+				var suppressions = new GlobalSuppressions ();
+				HashSet<ModuleDefinition> modules = new HashSet<ModuleDefinition> ();
+				foreach (var assembly in _context.Resolver.AssemblyCache.Values) {
+					DecodeGlobalSuppressMessageAttributes (assembly, suppressions);
+					modules.Add (assembly.MainModule);
+				}
+
+				foreach (var module in modules)
+					DecodeGlobalSuppressMessageAttributes (module, suppressions);
+
+				_globalSuppressions = suppressions;
+			}
+		}
+
+		private void DecodeGlobalSuppressMessageAttributes (ICustomAttributeProvider provider, GlobalSuppressions globalSuppressions)
+		{
+			var attributes = provider.CustomAttributes.Where (a => a.AttributeType.Name == "UnconditionalSuppressMessageAttribute");
+			foreach (var instance in attributes) {
+				SuppressMessageInfo info;
+				if (!TryDecodeSuppressMessageAttributeData (instance, out info))
+					continue;
+
+				if (info.Target == null && (info.Scope == "module" || info.Scope == null)) {
+					if (provider is ModuleDefinition)
+						AddLocalSuppression (instance, provider);
+					else if (provider is AssemblyDefinition assembly)
+						AddLocalSuppression (instance, assembly.MainModule);
+
+					continue;
+				}
+
+				switch (info.Scope) {
+				case "module":
+				case "type":
+				case "method":
+				case "member":
+					// Integrate Sven's work here
+					break;
+				case "resource":
+					// TODO
+					break;
+				case "namespace":
+				case "namespaceanddescendants":
+					// TODO
+					break;
+				}
+			}
+		}
+
+		private class GlobalSuppressions
+		{
+
 		}
 	}
 }
