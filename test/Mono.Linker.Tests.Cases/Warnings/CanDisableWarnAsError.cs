8a2d23ecb168eea76fc33744370c11c41ddf4bf5
Mateo Torres Ruiz <matorre@microsoft.com>
2020-07-21 22:29:56 -0700

    Add `--warnaserror` option

---
+++ test/Mono.Linker.Tests.Cases/Warnings/CanDisableWarnAsError.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.Warnings
+{
+	[SkipKeptItemsValidation]
+	[SetupLinkerSubstitutionFile ("CanDisableWarnAsErrorSubstitutions.xml")]
+	[SetupLinkerArgument ("--verbose")]
+	[SetupLinkerArgument ("--warnaserror")]
+	[SetupLinkerArgument ("--warnaserror-", "IL2011,IL2012,IgnoreThis")]
+	[LogContains ("error IL20(07|08|09|10)", true)]
+	[LogContains ("warning IL20(11|12)", true)]
+	public class CanDisableWarnAsError
+	{
+		public static void Main ()
+		{
+		}
+
+		class HelperClass
+		{
+			private int helperField = 0;
+			int HelperMethod ()
+			{
+				return 0;
+			}
+		}
+	}
+}
---
+++ test/Mono.Linker.Tests.Cases/Warnings/CanWarnAsError.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.Warnings
+{
+	[SkipKeptItemsValidation]
+	[SetupLinkerSubstitutionFile ("CanWarnAsErrorSubstitutions.xml")]
+	[SetupLinkerArgument ("--verbose")]
+	[SetupLinkerArgument ("--warnaserror-")]
+	[SetupLinkerArgument ("--warnaserror+", "IL2011,IgnoreThis")]
+	[SetupLinkerArgument ("--warnaserror", "IL2010,CS4321,IgnoreThisToo")]
+	[LogContains ("warning IL20(07|08|09|10)", true)]
+	[LogContains ("error IL20(11|12)", true)]
+	public class CanWarnAsError
+	{
+		public static void Main ()
+		{
+		}
+
+		class HelperClass
+		{
+			private int helperField = 0;
+			int HelperMethod ()
+			{
+				return 0;
+			}
+		}
+	}
+}
