dotnet/linker

commit 9c84659e8ff12a7af366a5f4d64ffc6c3623618c

Fix a couple nits (#1189)

src/linker/Linker.Steps/MarkStep.cs
@@ -1263,6 +1263,9 @@ namespace Mono.Linker.Steps
 				MarkMulticastDelegate (type);
 			}
 
+			if (type.IsClass && type.BaseType == null && type.Name == "Object")
+				MarkMethodIf (type.Methods, m => m.Name == "Finalize", new DependencyInfo (DependencyKind.MethodForSpecialType, type));
+
 			if (type.IsSerializable ())
 				MarkSerializable (type);
 
@@ -2238,7 +2241,7 @@ namespace Mono.Linker.Steps
 		protected virtual IEnumerable<MethodDefinition> GetRequiredMethodsForInstantiatedType (TypeDefinition type)
 		{
 			foreach (var method in type.Methods) {
-				if (method.IsFinalizer () || IsVirtualNeededByInstantiatedTypeDueToPreservedScope (method))
+				if (IsVirtualNeededByInstantiatedTypeDueToPreservedScope (method))
 					yield return method;
 			}
 		}

src/linker/Linker/MethodDefinitionExtensions.cs
@@ -71,6 +71,9 @@ namespace Mono.Linker
 			return method.IsConstructor && method.IsStatic;
 		}
 
+#if !FEATURE_ILLINK
+		// This implementation is wrong. It should return true if this is a virtual override
+		// of System.Object::Finalize, but that's not what this is doing. Do not use.
 		public static bool IsFinalizer (this MethodDefinition method)
 		{
 			if (method.Name != "Finalize" || method.ReturnType.MetadataType != MetadataType.Void)
@@ -81,6 +84,7 @@ namespace Mono.Linker
 
 			return true;
 		}
+#endif
 
 		public static void ClearDebugInformation (this MethodDefinition method)
 		{

src/linker/Linker/TypeDefinitionExtensions.cs
@@ -25,12 +25,12 @@ namespace Mono.Linker
 		public static TypeReference GetEnumUnderlyingType (this TypeDefinition enumType)
 		{
 			foreach (var field in enumType.Fields) {
-				if (!field.IsStatic && field.Name == "value__") {
+				if (!field.IsStatic) {
 					return field.FieldType;
 				}
 			}
 
-			throw new MissingFieldException ($"Enum type '{enumType.FullName}' is missing 'value__' field");
+			throw new MissingFieldException ($"Enum type '{enumType.FullName}' is missing instance field");
 		}
 
 		public static bool IsMulticastDelegate (this TypeDefinition td)

commit c5ced9d35a88eec2f9d4879c3f2e10073ddf32cc

Restore the ability to not mark System.Object.Finalize
Previously we overrode `GetRequiredMethodsForInstantiatedType` and used that to disable marking of Finalize during a special use case we have.

That broke with https://github.com/mono/linker/commit/9c84659e8ff12a7af366a5f4d64ffc6c3623618c

This PR adds a simple bool property to allow us to switch the new behavior that change added off.

src/linker/Linker.Steps/MarkStep.cs
@@ -1448,7 +1448,7 @@ namespace Mono.Linker.Steps
 				MarkMulticastDelegate (type);
 			}
 
-			if (type.IsClass && type.BaseType == null && type.Name == "Object")
+			if (type.IsClass && type.BaseType == null && type.Name == "Object" && ShouldMarkSystemObjectFinalize)
 				MarkMethodIf (type.Methods, m => m.Name == "Finalize", new DependencyInfo (DependencyKind.MethodForSpecialType, type), type);
 
 			if (type.IsSerializable ())
@@ -1520,6 +1520,11 @@ namespace Mono.Linker.Steps
 
 			return type;
 		}
+
+		/// <summary>
+		/// Allow subclasses to disable marking of System.Object.Finalize()
+		/// </summary>
+		protected virtual bool ShouldMarkSystemObjectFinalize => true;
 
 		// Allow subclassers to mark additional things in the main processing loop
 		protected virtual void DoAdditionalProcessing ()