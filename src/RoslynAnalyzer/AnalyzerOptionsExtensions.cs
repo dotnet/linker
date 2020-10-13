c0cff00ba72a69b364c3172a0ec59619edd39e16
Andy Gocke <andy@commentout.net>
2020-10-13 09:41:48 -0700

    Fix typos

--- src/RoslynAnalyzer/AnalyzerOptionsExtensions.cs
+++ src/RoslynAnalyzer/AnalyzerOptionsExtensions.cs
@@ -14,14 +14,6 @@ namespace ILTrimmingAnalyzer
             Compilation compilation,
             CancellationToken cancellationToken)
         {
-            // MSBuild property values should be set at compilation level, and cannot have different values per-tree.
-            // So, we default to first syntax tree.
-            var tree = compilation.SyntaxTrees.FirstOrDefault();
-            if (tree is null)
-            {
-                return null;
-            }
-
             return options.AnalyzerConfigOptionsProvider.GlobalOptions.TryGetValue(
                     $"build_property.{optionName}", out var value)
                 ? value
