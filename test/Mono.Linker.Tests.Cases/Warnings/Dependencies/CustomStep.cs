dc6ef476edc7ae44a6c7fb0a735ac18c10bb225a
Mateo Torres Ruiz <matorre@microsoft.com>
2020-11-06 10:48:31 -0800

    Fix formatting

--- test/Mono.Linker.Tests.Cases/Warnings/Dependencies/CustomStep.cs
+++ test/Mono.Linker.Tests.Cases/Warnings/Dependencies/CustomStep.cs
@@ -10,33 +10,33 @@ namespace CustomStep
 {
 	public class CustomStep : IStep
 	{
-		public void Process(LinkContext context)
+		public void Process (LinkContext context)
 		{
-			var warningMessage = MessageContainer.CreateCustomWarningMessage(
+			var warningMessage = MessageContainer.CreateCustomWarningMessage (
 				context: context,
 				text: "Warning",
 				code: 6001,
-				origin: new MessageOrigin(fileName: "CustomStep.cs"),
+				origin: new MessageOrigin (fileName: "CustomStep.cs"),
 				version: WarnVersion.Latest);
 
-			context.LogMessage(warningMessage);
+			context.LogMessage (warningMessage);
 		}
 	}
 
 	public class CustomStepWithInvalidWarningCode : IStep
 	{
-		public void Process(LinkContext context)
+		public void Process (LinkContext context)
 		{
 			// All codes in the range [1000-6000] are reserved for the linker
 			// and should not be used by external parties.
-			var invalidWarningMessage = MessageContainer.CreateCustomWarningMessage(
+			var invalidWarningMessage = MessageContainer.CreateCustomWarningMessage (
 				context: context,
 				text: "Warning",
 				code: 2500,
-				origin: new MessageOrigin(fileName: "CustomStep.cs"),
+				origin: new MessageOrigin (fileName: "CustomStep.cs"),
 				version: WarnVersion.Latest);
 
-			context.LogMessage(invalidWarningMessage);
+			context.LogMessage (invalidWarningMessage);
 		}
 	}
 }
