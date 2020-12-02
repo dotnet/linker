e19bc76d09c6e75ade92262eb10bb62af786091d
Tlakollo <tlcejava@microsoft.com>
2020-12-02 12:43:49 -0800

    -Remove unused include

--- src/ILLink.RoslynAnalyzer/RequiresUnreferencedCodeAnalyzer.cs
+++ src/ILLink.RoslynAnalyzer/RequiresUnreferencedCodeAnalyzer.cs
@@ -5,7 +5,6 @@
 using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
