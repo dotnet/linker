90ad9ecc54a7be7af895cfeb042e0a9f9feb6907
vitek-karas <vitek.karas@microsoft.com>
2020-12-08 13:41:57 -0800

    PR feedback
    Rewrite to specific checks - and explicit failure on function pointer (since that is probably not even handled in the runtime, let alone Roslyn and Cecil)

--- test/Mono.Linker.Tests.Cases/TypeForwarding/AttributeArgumentForwarded.cs
+++ test/Mono.Linker.Tests.Cases/TypeForwarding/AttributeArgumentForwarded.cs
@@ -85,6 +85,14 @@ namespace Mono.Linker.Tests.Cases.TypeForwarding
 		public static void Test_Field_ArrayOfPointerTypeRef ()
 		{
 		}
+
+		// This hits Roslyn bug https://github.com/dotnet/roslyn/issues/48765
+		//[Kept]
+		//[KeptAttributeAttribute (typeof (TestTypeAttribute))]
+		//[TestType (TestField = new object[] { typeof (delegate*<int, void>) })]
+		//public static void Test_Field_ArrayOfFunctionPointer ()
+		//{
+		//}
 	}
 
 	[KeptBaseType (typeof (Attribute))]
