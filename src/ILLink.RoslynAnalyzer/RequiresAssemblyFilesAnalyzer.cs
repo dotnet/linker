ceb902d4a206956969e534819cf81fac889db112
tlakollo <tlcejava@microsoft.com>
2021-03-29 19:04:14 -0700

    Merge SingleFileAnalyzer into RequiresAssemblyFilesAnalyzer

--- src/ILLink.RoslynAnalyzer/RequiresAssemblyFilesAnalyzer.cs
+++ src/ILLink.RoslynAnalyzer/RequiresAssemblyFilesAnalyzer.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
@@ -14,9 +15,34 @@ namespace ILLink.RoslynAnalyzer
 	[DiagnosticAnalyzer (LanguageNames.CSharp)]
 	public sealed class RequiresAssemblyFilesAnalyzer : DiagnosticAnalyzer
 	{
+		public const string IL3000 = nameof (IL3000);
+		public const string IL3001 = nameof (IL3001);
 		public const string IL3002 = nameof (IL3002);
+
 		internal const string RequiresAssemblyFilesAttribute = nameof (RequiresAssemblyFilesAttribute);
 		internal const string FullyQualifiedRequiresAssemblyFilesAttribute = "System.Diagnostics.CodeAnalysis." + RequiresAssemblyFilesAttribute;
+
+		static readonly DiagnosticDescriptor s_locationRule = new DiagnosticDescriptor (
+			IL3000,
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyLocationInSingleFileTitle),
+				Resources.ResourceManager, typeof (Resources)),
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyLocationInSingleFileMessage),
+				Resources.ResourceManager, typeof (Resources)),
+			DiagnosticCategory.SingleFile,
+			DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			helpLinkUri: "https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/il3000");
+
+		static readonly DiagnosticDescriptor s_getFilesRule = new DiagnosticDescriptor (
+			IL3001,
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyGetFilesInSingleFileTitle),
+				Resources.ResourceManager, typeof (Resources)),
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyGetFilesInSingleFileMessage),
+				Resources.ResourceManager, typeof (Resources)),
+			DiagnosticCategory.SingleFile,
+			DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			helpLinkUri: "https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/il3001");
 
 		static readonly DiagnosticDescriptor s_requiresAssemblyFilesRule = new DiagnosticDescriptor (
 			IL3002,
@@ -28,7 +54,7 @@ namespace ILLink.RoslynAnalyzer
 			DiagnosticSeverity.Warning,
 			isEnabledByDefault: true);
 
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (s_requiresAssemblyFilesRule);
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (s_locationRule, s_getFilesRule, s_requiresAssemblyFilesRule);
 
 		public override void Initialize (AnalysisContext context)
 		{
@@ -46,9 +72,36 @@ namespace ILLink.RoslynAnalyzer
 				if (string.Equals (includesAllContent?.Trim (), "true", StringComparison.OrdinalIgnoreCase))
 					return;
 
+				var propertiesBuilder = ImmutableArray.CreateBuilder<IPropertySymbol> ();
+				var methodsBuilder = ImmutableArray.CreateBuilder<IMethodSymbol> ();
+
+				var assemblyType = compilation.GetTypeByMetadataName ("System.Reflection.Assembly");
+				if (assemblyType != null) {
+					// properties
+					AddIfNotNull (propertiesBuilder, TryGetSingleSymbol<IPropertySymbol> (assemblyType.GetMembers ("Location")));
+
+					// methods
+					methodsBuilder.AddRange (assemblyType.GetMembers ("GetFile").OfType<IMethodSymbol> ());
+					methodsBuilder.AddRange (assemblyType.GetMembers ("GetFiles").OfType<IMethodSymbol> ());
+				}
+
+				var assemblyNameType = compilation.GetTypeByMetadataName ("System.Reflection.AssemblyName");
+				if (assemblyNameType != null) {
+					AddIfNotNull (propertiesBuilder, TryGetSingleSymbol<IPropertySymbol> (assemblyNameType.GetMembers ("CodeBase")));
+					AddIfNotNull (propertiesBuilder, TryGetSingleSymbol<IPropertySymbol> (assemblyNameType.GetMembers ("EscapedCodeBase")));
+				}
+
+				var properties = propertiesBuilder.ToImmutable ();
+				var methods = methodsBuilder.ToImmutable ();
+
 				context.RegisterOperationAction (operationContext => {
 					var methodInvocation = (IInvocationOperation) operationContext.Operation;
-					CheckCalledMember (operationContext, methodInvocation.TargetMethod);
+					var targetMethod = methodInvocation.TargetMethod;
+					if (!operationContext.ContainingSymbol.HasAttribute (RequiresAssemblyFilesAttribute) && Contains (methods, targetMethod, SymbolEqualityComparer.Default)) {
+						operationContext.ReportDiagnostic (Diagnostic.Create (s_getFilesRule, methodInvocation.Syntax.GetLocation (), targetMethod));
+						return;
+					}
+					CheckCalledMember (operationContext, targetMethod);
 				}, OperationKind.Invocation);
 
 				context.RegisterOperationAction (operationContext => {
@@ -59,6 +112,10 @@ namespace ILLink.RoslynAnalyzer
 				context.RegisterOperationAction (operationContext => {
 					var propAccess = (IPropertyReferenceOperation) operationContext.Operation;
 					var prop = propAccess.Property;
+					if (!operationContext.ContainingSymbol.HasAttribute (RequiresAssemblyFilesAttribute) && Contains (properties, prop, SymbolEqualityComparer.Default)) {
+						operationContext.ReportDiagnostic (Diagnostic.Create (s_locationRule, propAccess.Syntax.GetLocation (), prop));
+						return;
+					}
 					var usageInfo = propAccess.GetValueUsageInfo (prop);
 					if (usageInfo.HasFlag (ValueUsageInfo.Read) && prop.GetMethod != null)
 						CheckCalledMember (operationContext, prop.GetMethod);
@@ -93,6 +150,39 @@ namespace ILLink.RoslynAnalyzer
 							member.OriginalDefinition.ToString (),
 							message,
 							url));
+					}
+				}
+
+				static bool Contains<T, TComp> (ImmutableArray<T> list, T elem, TComp comparer)
+					where TComp : IEqualityComparer<T>
+				{
+					foreach (var e in list) {
+						if (comparer.Equals (e, elem)) {
+							return true;
+						}
+					}
+					return false;
+				}
+
+				static TSymbol? TryGetSingleSymbol<TSymbol> (ImmutableArray<ISymbol> members) where TSymbol : class, ISymbol
+				{
+					TSymbol? candidate = null;
+					foreach (var m in members) {
+						if (m is TSymbol tsym) {
+							if (candidate is null) {
+								candidate = tsym;
+							} else {
+								return null;
+							}
+						}
+					}
+					return candidate;
+				}
+
+				static void AddIfNotNull<TSymbol> (ImmutableArray<TSymbol>.Builder properties, TSymbol? p) where TSymbol : class, ISymbol
+				{
+					if (p != null) {
+						properties.Add (p);
 					}
 				}
 			});
