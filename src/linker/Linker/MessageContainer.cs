67b7026f9f2cde252008f8a4c51f2162318b7570
Sven Boemer <sbomer@gmail.com>
2021-04-05 11:31:09 -0700

    Check subcategory, assert for MemberDefinition

--- src/linker/Linker/MessageContainer.cs
+++ src/linker/Linker/MessageContainer.cs
@@ -132,10 +132,11 @@ namespace Mono.Linker
 			if (version > context.WarnVersion)
 				return Empty;
 
-			if (origin.MemberDefinition != null) {
-				var assembly = origin.MemberDefinition.DeclaringType.Module.Assembly;
-				var assemblyName = assembly.Name.Name;
-				if (context.IsSingleWarn (assemblyName)) {
+			if (subcategory == MessageSubCategory.TrimAnalysis) {
+				Debug.Assert (origin.MemberDefinition != null);
+				var assembly = origin.MemberDefinition?.DeclaringType.Module.Assembly;
+				var assemblyName = assembly?.Name.Name;
+				if (assemblyName != null && context.IsSingleWarn (assemblyName)) {
 					if (context.AssembliesWithGeneratedSingleWarning.Add (assemblyName))
 						context.LogWarning ($"Assembly '{assemblyName}' produced trim warnings. For more information see https://aka.ms/dotnet-illink/libraries", 2104, context.GetAssemblyLocation (assembly));
 					return Empty;
