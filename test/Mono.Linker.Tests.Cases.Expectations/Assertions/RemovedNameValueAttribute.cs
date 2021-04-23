c0721b678cbedc38bf8eaef9c73597f7d87fc301
Marek Safar <marek.safar@gmail.com>
2021-04-23 14:38:05 +0200

    Update test/Mono.Linker.Tests.Cases.Expectations/Assertions/RemovedNameValueAttribute.cs
    Co-authored-by: Vitek Karas <vitek.karas@microsoft.com>

--- test/Mono.Linker.Tests.Cases.Expectations/Assertions/RemovedNameValueAttribute.cs
+++ test/Mono.Linker.Tests.Cases.Expectations/Assertions/RemovedNameValueAttribute.cs
@@ -3,7 +3,7 @@
 namespace Mono.Linker.Tests.Cases.Expectations.Assertions
 {
 	/// <summary>
-	/// Verifies that literal value of the member is removed
+	/// Verifies that name of the member is removed
 	/// </summary>
 	[AttributeUsage (AttributeTargets.All, AllowMultiple = false, Inherited = false)]
 	public class RemovedNameValueAttribute : BaseExpectedLinkedBehaviorAttribute
