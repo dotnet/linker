35d4d561db5f163a4ebba0e1b86bd2fa02cf2e54
Marek Safar <marek.safar@gmail.com>
2021-05-15 12:59:16 +0200

    Remove custom marking of nested types for event sources
    Fixes #1949

--- src/linker/Linker.Steps/MarkStep.cs
+++ src/linker/Linker.Steps/MarkStep.cs
@@ -1697,7 +1697,7 @@ namespace Mono.Linker.Steps
 			MarkSerializable (type);
 
 			// This marks static fields of KeyWords/OpCodes/Tasks subclasses of an EventSource type.
-			if (BCL.EventTracingForWindows.IsEventSourceImplementation (type, _context)) {
+			if (_context.GetTargetRuntimeVersion () < TargetRuntimeVersion.NET6 && BCL.EventTracingForWindows.IsEventSourceImplementation (type, _context)) {
 				MarkEventSourceProviders (type);
 			}
 
