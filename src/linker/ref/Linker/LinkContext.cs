383a6a417e9e511f4aefec28f1d050a7fa662390
Sven Boemer <sbomer@gmail.com>
2021-06-01 19:59:21 -0700

    Fix using order

--- src/linker/ref/Linker/LinkContext.cs
+++ src/linker/ref/Linker/LinkContext.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Mono.Cecil;
 using System;
+using Mono.Cecil;
 
 namespace Mono.Linker
 {
