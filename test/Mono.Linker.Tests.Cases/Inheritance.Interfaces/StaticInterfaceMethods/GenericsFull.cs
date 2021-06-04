0c883074f0e816223f5dd1ee78dc259dcc434486
Lakshan Fernando <lakshanf@microsoft.com>
2021-06-04 14:23:34 -0700

    test updates

---
+++ test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/StaticInterfaceMethods/GenericsFull.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.Inheritance.Interfaces.StaticInterfaceMethods
+{
+	[SetupLinkerArgument ("--skip-unresolved", "true")]
+#if !NETCOREAPP
+	[IgnoreTestCase("Only for .NET Core for some reason")]
+#endif
+	[Define ("IL_ASSEMBLY_AVAILABLE")]
+	[SetupCompileBefore ("library.dll", new[] { "Dependencies/GenericsFull.il" })]
+	[KeptTypeInAssembly ("library.dll", "IFaceNonGeneric")]
+	[KeptTypeInAssembly ("library.dll", "IFaceGeneric`1")]
+	[KeptTypeInAssembly ("library.dll", "IFaceCuriouslyRecurringGeneric`1")]
+
+	class GenericsFull
+	{
+		static void Main ()
+		{
+#if IL_ASSEMBLY_AVAILABLE
+			TestEntrypoint.Test();
+#endif
+		}
+	}
+}
--- test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/StaticInterfaceMethods/VarianceBasic.cs
+++ test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/StaticInterfaceMethods/VarianceBasic.cs
@@ -14,7 +14,11 @@ namespace Mono.Linker.Tests.Cases.Inheritance.Interfaces.StaticInterfaceMethods
 	[SetupCompileBefore ("library.dll", new[] { "Dependencies/VarianceBasic.il" })]
 
 	[KeptTypeInAssembly ("library.dll", "InterfaceScenario1`1")]
+	[KeptTypeInAssembly ("library.dll", "InterfaceScenario2`1")]
 	[KeptMemberInAssembly ("library.dll", "BaseScenario1", "Method()")]
+	[KeptMemberInAssembly ("library.dll", "BaseScenario2", "Method()", "Method2()", "Method_Obj()", "Method2_Obj()")]
+	[KeptTypeInAssembly ("library.dll", "DerivedScenario1")]
+	[KeptMemberInAssembly ("library.dll", "DerivedScenario2", "Method()", "Method2()")]
 
 	class VarianceBasic
 	{
