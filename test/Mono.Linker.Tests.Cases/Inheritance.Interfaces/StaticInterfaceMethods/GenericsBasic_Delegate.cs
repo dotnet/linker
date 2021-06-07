ec5a17565ab246e38e5df87f39a99524062ba6f6
Lakshan Fernando <lakshanf@microsoft.com>
2021-06-07 04:33:06 -0700

    fixed tests

--- test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/StaticInterfaceMethods/GenericsBasic_Delegate.cs
+++ test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/StaticInterfaceMethods/GenericsBasic_Delegate.cs
@@ -13,8 +13,7 @@ namespace Mono.Linker.Tests.Cases.Inheritance.Interfaces.StaticInterfaceMethods
 	[Define ("IL_ASSEMBLY_AVAILABLE")]
 	[SetupCompileBefore ("library.dll", new[] { "Dependencies/GenericsBasic_Delegate.il" })]
 	[KeptTypeInAssembly ("library.dll", "IFaceNonGeneric")]
-	[KeptMemberInAssembly ("library.dll", "NonGenericClass", "IFaceNonGeneric.NormalMethod()")]
-	[KeptMemberInAssembly ("library.dll", "NonGenericClass", "IFaceNonGeneric.GenericMethod<#1>()")]
+	[KeptMemberInAssembly ("library.dll", "NonGenericClass", "IFaceNonGeneric.NormalMethod()", "IFaceNonGeneric.GenericMethod<#1>()")]
 
 	class GenericsBasic_Delegate
 	{
