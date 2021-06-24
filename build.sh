5550c01c2474303a0cbac1cbfd2a3987b1e037d2
Andy Gocke <angocke@microsoft.com>
2021-06-23 22:44:07 -0700

    Add bootstrap run on Linux
    Adds support for a bootstrap flag on Linux, which builds the linker uses
    it to trim a test copy of csc. The trimmed csc is then used to build the
    linker and tests. This should act as a real-world trimming test.

--- build.sh
+++ build.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+
+# Stop script if subcommand fails
+set -e
+
+bootstrap=false
+
+args=""
+while [[ $# > 0 ]]; do
+  opt="$(echo "$1" | awk '{print tolower($0)}')"
+  case "$opt" in
+    --bootstrap)
+      bootstrap=true
+      shift
+      ;;
+    *)
+    args="$args $1"
+    shift
+    ;;
+  esac
+done
+
 source="${BASH_SOURCE[0]}"
 
 # resolve $SOURCE until the file is no longer a symlink
@@ -11,6 +34,42 @@ while [[ -h $source ]]; do
   # symlink file was located
   [[ $source != /* ]] && source="$scriptroot/$source"
 done
-
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-"$scriptroot/eng/common/build.sh" --build --restore $@
+
+# Import Arcade functions
+. "$scriptroot/eng/common/tools.sh"
+
+function BuildBootstrapCompiler {
+  InitializeDotNetCli true
+  # Build linker
+  echo "Building illink"
+
+  local dir="$artifacts_dir/bootstrap"
+  local illink_dir="$dir/illink"
+
+  rm -rf $dir
+  mkdir -p $dir
+
+  local illink_proj="ILLink.Tasks"
+  local illink_proj_path="src/$illink_proj/$illink_proj.csproj"
+  dotnet pack -nologo "$illink_proj_path" -p:PackageOutputPath="$dir"
+  unzip "$dir/Microsoft.NET.$illink_proj.*.nupkg" -d "$illink_dir"
+  chmod -R 755 "$dir"
+
+  # Build csc with trimming
+  echo "Building csc"
+  local csc_proj_path="test/TestCsc/csc.csproj"
+  dotnet publish -nologo --use-current-runtime -o "$dir/csc" "$csc_proj_path" -p:BootstrapLinkerPath="$illink_dir" -bl:"$log_dir/BootstrapCsc.binlog"
+
+  echo "Cleaning bootstrap artifacts"
+  dotnet clean -nologo -v:m "$illink_proj_path"
+  dotnet clean -nologo -v:m "$csc_proj_path"
+
+  _BootstrapCompilerPath="$dir/csc"
+}
+
+if [[ "$bootstrap" == true ]]; then
+  BuildBootstrapCompiler
+fi
+
+"$scriptroot/eng/common/build.sh" --build --restore -p:BootstrapCscPath="${_BootstrapCompilerPath:-}" $args
---
+++ test/TestCsc/Program.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Microsoft.CodeAnalysis.CSharp.CommandLine
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var clientDir = AppContext.BaseDirectory;
+            var workingDir = Directory.GetCurrentDirectory();
+            var tempDir = Path.GetTempPath();
+            var textWriter = Console.Out;
+	        return Csc.Run(
+                args,
+                new BuildPaths (clientDir: clientDir, workingDir: workingDir, sdkDir: null, tempDir: tempDir),
+                textWriter,
+                new NoopLoader());
+        }
+    }
+
+	internal sealed class NoopLoader : IAnalyzerAssemblyLoader
+	{
+	    public void AddDependencyLocation (string fullPath) { }
+
+	    public Assembly? LoadFromPath (string fullPath) => null;
+    }
+}
