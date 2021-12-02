21c5165fc26392ac296a10ea0e7891681af93a6d
Jackson Schuster <jschuster@microsoft.com>
2021-12-02 17:03:25 -0600

    Reverting to using IsMemberInRequiresScope

--- test/ILLink.RoslynAnalyzer.Tests/RequiresUnreferencedCodeAnalyzerTests.cs
+++ test/ILLink.RoslynAnalyzer.Tests/RequiresUnreferencedCodeAnalyzerTests.cs
@@ -408,5 +408,31 @@ class C
 
 			return VerifyRequiresUnreferencedCodeAnalyzer (source);
 		}
+	
+
+		[Fact]
+		public Task BaseRucMethodDerivedRucClass ()
+		{
+			var source = @"
+	using System.Diagnostics.CodeAnalysis;
+
+	class BaseWithoutRequiresOnType
+	{
+		[RequiresUnreferencedCode (""RUC"")]
+		public virtual void Method () { }
+	}
+
+	[RequiresUnreferencedCode (""RUC"")]
+	class DerivedWithRequiresOnType : BaseWithoutRequiresOnType
+	{
+		public override void Method () { }
+	}
+	";
+
+			return VerifyRequiresUnreferencedCodeAnalyzer (source);
+		}
 	}
 }
+
+
+
