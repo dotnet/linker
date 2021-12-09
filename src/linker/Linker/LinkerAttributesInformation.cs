db5f6fd33d8af3ff9c96c376733289a8e1213d4a
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2021-12-09 12:05:23 -0600

    Remove Unnecessary usings

--- src/linker/Linker/LinkerAttributesInformation.cs
+++ src/linker/Linker/LinkerAttributesInformation.cs
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using Mono.Cecil;
 
 namespace Mono.Linker
