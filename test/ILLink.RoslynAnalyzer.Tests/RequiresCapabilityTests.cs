9265f897732965434b55c69f7491542b40d99b1a
tlakollo <tlakaelel_axayakatl@outlook.com>
2022-01-14 23:54:42 -0800

    Divide RequiresCapability file into several testing files to have a better debug experience

--- test/ILLink.RoslynAnalyzer.Tests/RequiresCapabilityTests.cs
+++ test/ILLink.RoslynAnalyzer.Tests/RequiresCapabilityTests.cs
@@ -9,9 +9,21 @@ namespace ILLink.RoslynAnalyzer.Tests
 		protected override string TestSuiteName => "RequiresCapability";
 
 		[Fact]
-		public Task RequiresCapability ()
+		public Task BasicRequires ()
 		{
-			return RunTest (nameof (RequiresCapability));
+			return RunTest (nameof (BasicRequires));
+		}
+
+		[Fact]
+		public Task RequiresAccessedThrough ()
+		{
+			return RunTest (nameof (RequiresAccessedThrough));
+		}
+
+		[Fact]
+		public Task RequiresAttributeMismatch ()
+		{
+			return RunTest (nameof (RequiresAttributeMismatch));
 		}
 
 		[Fact]
@@ -27,15 +39,57 @@ namespace ILLink.RoslynAnalyzer.Tests
 		}
 
 		[Fact]
+		public Task RequiresCopyAssembly ()
+		{
+			return RunTest (nameof (RequiresCopyAssembly));
+		}
+
+		[Fact]
 		public Task RequiresInCompilerGeneratedCode ()
 		{
 			return RunTest (nameof (RequiresInCompilerGeneratedCode));
 		}
 
 		[Fact]
+		public Task RequiresOnAttribute ()
+		{
+			return RunTest (nameof (RequiresOnAttribute));
+		}
+
+		[Fact]
 		public Task RequiresOnAttributeCtor ()
 		{
 			return RunTest (nameof (RequiresOnAttributeCtor));
+		}
+
+		[Fact]
+		public Task RequiresOnClass ()
+		{
+			return RunTest (nameof (RequiresOnClass));
+		}
+
+		[Fact]
+		public Task RequiresOnStaticConstructor ()
+		{
+			return RunTest (nameof (RequiresOnStaticConstructor));
+		}
+
+		[Fact]
+		public Task RequiresViaDataflow ()
+		{
+			return RunTest (nameof (RequiresViaDataflow));
+		}
+
+		[Fact]
+		public Task RequiresViaXml ()
+		{
+			return RunTest (nameof (RequiresViaXml));
+		}
+
+		[Fact]
+		public Task SuppressRequires ()
+		{
+			return RunTest (nameof (SuppressRequires));
 		}
 	}
 }
