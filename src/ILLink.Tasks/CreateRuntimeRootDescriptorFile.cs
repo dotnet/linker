fe36b380e00bd4c12c99e461a4e7eeab77dd7d95
Jackson Schuster <jschuster@microsoft.com>
2022-01-19 15:49:43 -0800

    Rename "none" to "nothing" in CreateRuntimeRootILLinkDescriptorFile

--- src/ILLink.Tasks/CreateRuntimeRootDescriptorFile.cs
+++ src/ILLink.Tasks/CreateRuntimeRootDescriptorFile.cs
@@ -356,7 +356,7 @@ namespace ILLink.Tasks
 					typeNode.Attributes.Append (preserve);
 				} else if ((members.fields == null) && (members.methods == null)) {
 					XmlAttribute preserve = doc.CreateAttribute ("preserve");
-					preserve.Value = "none";
+					preserve.Value = "nothing";
 					typeNode.Attributes.Append (preserve);
 				}
 
