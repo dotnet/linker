5282fc63880cde7c119aa224b2f02a9186ddf414
Andy Gocke <angocke@microsoft.com>
2022-01-24 21:12:31 -0800

    Revert unnecessary changes

--- build.sh
+++ build.sh
@@ -1,26 +1,6 @@
 #!/usr/bin/env bash
 
-# Stop script if unbound variable found (use ${var:-} if intentional)
-set -u
-
-# Stop script if subcommand fails
-set -e
-
 source="${BASH_SOURCE[0]}"
-
-bootstrap=false
-
-args=""
-while [[ $# > 0 ]]; do
-  opt="$(echo "${1/#--/-}" | tr "[:upper:]" "[:lower:]")"
-  case "$opt" in
-    -integrationtest)
-      bootstrap=true
-      ;;
-  esac
-  args="$args $1"
-  shift
-done
 
 # resolve $SOURCE until the file is no longer a symlink
 while [[ -h $source ]]; do
