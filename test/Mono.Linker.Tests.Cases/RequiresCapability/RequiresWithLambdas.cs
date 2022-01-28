f000e6215be430d53da19e4e6032a296e814e807
Jackson Schuster <jschuster@microsoft.com>
2022-01-28 10:49:10 -0800

    Found linker reports diags on lambdas as their generated methods

--- test/Mono.Linker.Tests.Cases/RequiresCapability/RequiresWithLambdas.cs
+++ test/Mono.Linker.Tests.Cases/RequiresCapability/RequiresWithLambdas.cs
@@ -15,10 +15,14 @@ namespace Mono.Linker.Tests.Cases.RequiresCapability
 {
 	[SkipKeptItemsValidation]
 	[ExpectedNoWarnings]
+	[LogContains("IL2026: Mono.Linker.Tests.Cases.RequiresCapability.RequiresWithLambdas.NoRequiresOnClass.<InvokeLocalRucAction>b__1_0(): Using member 'Mono.Linker.Tests.Cases.RequiresCapability.RequiresWithLambdas.NoRequiresOnClass.RucMethod()' which has 'RequiresUnreferencedCodeAttribute' can break functionality when trimming application code. RucMethod.", ProducedBy = ProducedBy.Trimmer)]
+	[LogContains("IL2026: Mono.Linker.Tests.Cases.RequiresCapability.RequiresWithLambdas.NoRequiresOnClass.<ReturnRucAction>b__2_0(): Using member 'Mono.Linker.Tests.Cases.RequiresCapability.RequiresWithLambdas.NoRequiresOnClass.RucMethod()' which has 'RequiresUnreferencedCodeAttribute' can break functionality when trimming application code. RucMethod.", ProducedBy = ProducedBy.Trimmer)]
 	public class RequiresWithLambdas
 	{
-		[ExpectedWarning("IL2026", nameof(RequiresOnClass))]
-		[ExpectedWarning("IL2026", nameof(NoRequiresOnClass))]
+		[ExpectedWarning("IL2026", nameof(RequiresOnClass), ProducedBy = ProducedBy.Trimmer)]
+		[ExpectedWarning("IL2026", nameof(NoRequiresOnClass.RucMethod), ProducedBy = ProducedBy.Trimmer)]
+		[ExpectedWarning("IL2026", nameof(NoRequiresOnClass.ReturnRucAction), ProducedBy = ProducedBy.Trimmer)]
+		[ExpectedWarning("IL2026", nameof(NoRequiresOnClass.PassRucMethodToBeInvoked), ProducedBy = ProducedBy.Trimmer)]
 		public static void Main ()
 		{
 			typeof (RequiresOnClass).RequiresAll ();
@@ -28,7 +32,6 @@ namespace Mono.Linker.Tests.Cases.RequiresCapability
 		[RequiresUnreferencedCode (nameof (RequiresOnClass))]
 		public class RequiresOnClass
 		{
-			public delegate void action ();
 			public void RucMethod () { }
 			public void InvokeLocalRucAction ()
 			{
@@ -47,7 +50,7 @@ namespace Mono.Linker.Tests.Cases.RequiresCapability
 				f ();
 			}
 
-			public void InvokeActionArg (action f)
+			public void InvokeActionArg (Action f)
 			{
 				f ();
 			}
@@ -60,8 +63,6 @@ namespace Mono.Linker.Tests.Cases.RequiresCapability
 
 		public class NoRequiresOnClass
 		{
-			public delegate void action ();
-
 			[RequiresUnreferencedCode (nameof (RucMethod))]
 			public void RucMethod () { }
 
@@ -81,14 +82,14 @@ namespace Mono.Linker.Tests.Cases.RequiresCapability
 			}
 
 			// Bug - Should warn in Trimmer
-			[ExpectedWarning ("IL2026", nameof (ReturnRucAction), ProducedBy = ProducedBy.Analyzer)]
+			[ExpectedWarning ("IL2026", nameof (ReturnRucAction))]
 			public void InvokeReturnedRucAction ()
 			{
 				var f = ReturnRucAction ();
 				f ();
 			}
 
-			public void InvokeActionArg (action f)
+			public void InvokeActionArg (Action f)
 			{
 				f ();
 			}
@@ -97,6 +98,7 @@ namespace Mono.Linker.Tests.Cases.RequiresCapability
 			public void PassRucMethodToBeInvoked ()
 			{
 				InvokeActionArg (RucMethod);
+				InvokeActionArg (ReturnRucAction ());
 			}
 		}
 
