c95d7853a91376f343c9c0b08a3bd2060a543b48
Jackson Schuster <jschuster@microsoft.com>
2022-02-25 13:40:25 -0800

    Move repro to an existing test and rename classes

--- test/Mono.Linker.Tests.Cases/ComponentModel/TypeDescriptionProviderAttributeOnType.cs
+++ test/Mono.Linker.Tests.Cases/ComponentModel/TypeDescriptionProviderAttributeOnType.cs
@@ -1,37 +1,70 @@
 using System.ComponentModel;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
+using System.Diagnostics.CodeAnalysis;
+using System;
+
 
 namespace Mono.Linker.Tests.Cases.ComponentModel
 {
-	[TypeDescriptionProvider (typeof (CustomTDP))]
-
-	[Kept]
-	[KeptAttributeAttribute (typeof (TypeDescriptionProviderAttribute))]
-	class CustomTypeDescriptionProvider_1
-	{
-		[Kept]
-		public CustomTypeDescriptionProvider_1 ()
-		{
-		}
-
-		[Kept]
-		[KeptBaseType (typeof (TypeDescriptionProvider))]
-		class CustomTDP : TypeDescriptionProvider
-		{
-			[Kept]
-			public CustomTDP ()
-			{
-			}
-		}
-	}
 
 	[Reference ("System.dll")]
+	[ExpectedNoWarnings]
 	public class TypeDescriptionProviderAttributeOnType
 	{
 		public static void Main ()
 		{
 			var r1 = new CustomTypeDescriptionProvider_1 ();
+			IInterface v = InterfaceTypeConverter.CreateVisual (typeof (System.String));
+		}
+
+		[TypeDescriptionProvider (typeof (CustomTDP))]
+
+		[Kept]
+		[KeptAttributeAttribute (typeof (TypeDescriptionProviderAttribute))]
+		class CustomTypeDescriptionProvider_1
+		{
+			[Kept]
+			public CustomTypeDescriptionProvider_1 ()
+			{
+			}
+
+			[Kept]
+			[KeptBaseType (typeof (TypeDescriptionProvider))]
+			class CustomTDP : TypeDescriptionProvider
+			{
+				[Kept]
+				public CustomTDP ()
+				{
+				}
+			}
+		}
+		[Kept]
+		[KeptAttributeAttribute (typeof (TypeConverterAttribute))]
+		[TypeConverter (typeof (InterfaceTypeConverter))]
+		public interface IInterface
+		{ }
+
+		[Kept]
+		[KeptBaseType (typeof (TypeConverter))]
+		public class InterfaceTypeConverter : TypeConverter
+		{
+			[Kept]
+			public static IInterface CreateVisual (
+			[KeptAttributeAttribute (typeof (DynamicallyAccessedMembersAttribute))]
+				[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
+				Type visualType)
+			{
+				try {
+					return (IInterface) Activator.CreateInstance (visualType);
+				} catch {
+				}
+
+				return null;
+			}
+
+			[Kept]
+			public InterfaceTypeConverter () { }
 		}
 	}
 }
