74d7294f892780fef8a6a344a5e775a9cd04f41e
Jackson Schuster <jschuster@microsoft.com>
2022-03-07 13:45:00 -0800

    Minimal repro

---
+++ test/Mono.Linker.Tests.Cases/DataFlow/BackingFieldKept.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Helpers;
+
+namespace Mono.Linker.Tests.Cases.DataFlow
+{
+	class BackingFieldKept
+	{
+		static void Main ()
+		{
+			typeof (Class).RequiresPublicProperties ();
+			typeof (Struct).RequiresPublicProperties ();
+		}
+
+		struct Class
+		{
+			[Kept]
+			[KeptBackingField]
+			public string FirstName { [Kept]get; [Kept]set; }
+			[Kept]
+			[KeptBackingField]
+			public string LastName { [Kept]get; [Kept]set; }
+		}
+		struct Struct
+		{
+			[Kept]
+			[KeptBackingField]
+			public string FirstName { [Kept]get; [Kept]set; }
+			[Kept]
+			[KeptBackingField]
+			public string LastName { [Kept]get; [Kept]set; }
+		}
+	}
+}
