65cd97a7522b9fc9e47ab5390cf030314bfc1563
Jackson Schuster <jschuster@microsoft.com>
2022-03-17 14:27:00 -0700

    Use switch expressions instead of statements

--- src/ILLink.RoslynAnalyzer/TrimAnalysis/WellKnownTypeExtensions.cs
+++ src/ILLink.RoslynAnalyzer/TrimAnalysis/WellKnownTypeExtensions.cs
@@ -1,31 +1,23 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis;
 
 namespace ILLink.Shared.TypeSystemProxy
 {
 	public static partial class WellKnownTypeExtensions
 	{
-		public static bool TryGetSpecialType (this WellKnownType wellKnownType, out SpecialType? specialType)
+		public static bool TryGetSpecialType (this WellKnownType wellKnownType, [NotNullWhen (true)] out SpecialType? specialType)
 		{
-			switch (wellKnownType) {
-			case WellKnownType.System_String:
-				specialType = SpecialType.System_String;
-				return true;
-			case WellKnownType.System_Nullable_T:
-				specialType = SpecialType.System_Nullable_T;
-				return true;
-			case WellKnownType.System_Array:
-				specialType = SpecialType.System_Array;
-				return true;
-			case WellKnownType.System_Object:
-				specialType = SpecialType.System_Object;
-				return true;
-			default:
-				specialType = null;
-				return false;
-			}
+			specialType = wellKnownType switch {
+				WellKnownType.System_String => SpecialType.System_String,
+				WellKnownType.System_Nullable_T => SpecialType.System_Nullable_T,
+				WellKnownType.System_Array => SpecialType.System_Array,
+				WellKnownType.System_Object => SpecialType.System_Object,
+				_ => null,
+			};
+			return specialType is not null;
 		}
 	}
 }
--- src/ILLink.Shared/TypeSystemProxy/WellKnownType.cs
+++ src/ILLink.Shared/TypeSystemProxy/WellKnownType.cs
@@ -1,4 +1,3 @@
-
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
@@ -21,24 +20,16 @@ namespace ILLink.Shared.TypeSystemProxy
 	{
 		public static (string Namespace, string Name) GetNamespaceAndName (this WellKnownType type)
 		{
-			switch (type) {
-			case WellKnownType.System_String:
-				return ("System", "String");
-			case WellKnownType.System_Nullable_T:
-				return ("System", "Nullable`1");
-			case WellKnownType.System_Type:
-				return ("System", "Type");
-			case WellKnownType.System_Reflection_IReflect:
-				return ("System.Reflection", "IReflect");
-			case WellKnownType.System_Array:
-				return ("System", "Array");
-			case WellKnownType.System_Object:
-				return ("System", "Object");
-			case WellKnownType.System_Attribute:
-				return ("System", "Attribute");
-			default:
-				throw new ArgumentException ($"{nameof (type)} is not a well-known type.");
-			}
+			return type switch {
+				WellKnownType.System_String => ("System", "String"),
+				WellKnownType.System_Nullable_T => ("System", "Nullable`1"),
+				WellKnownType.System_Type => ("System", "Type"),
+				WellKnownType.System_Reflection_IReflect => ("System.Reflection", "IReflect"),
+				WellKnownType.System_Array => ("System", "Array"),
+				WellKnownType.System_Object => ("System", "Object"),
+				WellKnownType.System_Attribute => ("System", "Attribute"),
+				_ => throw new ArgumentException ($"{nameof (type)} is not a well-known type."),
+			};
 		}
 		public static string GetNamespace (this WellKnownType type) => GetNamespaceAndName (type).Namespace;
 		public static string GetName (this WellKnownType type) => GetNamespaceAndName (type).Name;
