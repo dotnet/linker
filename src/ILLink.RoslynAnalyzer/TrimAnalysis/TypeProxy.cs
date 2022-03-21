29833f322ae9a9f30ccc37f780d0df35af8b1b55
Jackson Schuster <jschuster@microsoft.com>
2022-03-21 11:56:27 -0700

    Inline simple wrapper functions in TypeProxy

--- src/ILLink.RoslynAnalyzer/TrimAnalysis/TypeProxy.cs
+++ src/ILLink.RoslynAnalyzer/TrimAnalysis/TypeProxy.cs
@@ -1,8 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.CompilerServices;
 using ILLink.RoslynAnalyzer;
 using Microsoft.CodeAnalysis;
+using static System.Runtime.CompilerServices.MethodImplOptions;
 
 namespace ILLink.Shared.TypeSystemProxy
 {
@@ -16,12 +18,16 @@ namespace ILLink.Shared.TypeSystemProxy
 
 		public string? Namespace { get => Type.ContainingNamespace?.Name; }
 
+		[MethodImpl(AggressiveInlining)]
 		public bool IsTypeOf (string @namespace, string name) => Type.IsTypeOf (@namespace, name);
 
+		[MethodImpl(AggressiveInlining)]
 		public bool IsTypeOf (WellKnownType wellKnownType) => Type.IsTypeOf (wellKnownType);
 
+		[MethodImpl(AggressiveInlining)]
 		public string GetDisplayName () => Type.GetDisplayName ();
 
+		[MethodImpl(AggressiveInlining)]
 		public override string ToString () => Type.ToString ();
 	}
 }
--- src/linker/Linker.Dataflow/TypeProxy.cs
+++ src/linker/Linker.Dataflow/TypeProxy.cs
@@ -1,8 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.CompilerServices;
 using Mono.Cecil;
 using Mono.Linker;
+using static System.Runtime.CompilerServices.MethodImplOptions;
 
 namespace ILLink.Shared.TypeSystemProxy
 {
@@ -18,12 +20,16 @@ namespace ILLink.Shared.TypeSystemProxy
 
 		public string? Namespace { get => Type.Namespace; }
 
+		[MethodImpl(AggressiveInlining)]
 		public bool IsTypeOf (string @namespace, string name) => Type.IsTypeOf (@namespace, name);
 
+		[MethodImpl(AggressiveInlining)]
 		public bool IsTypeOf (WellKnownType wellKnownType) => Type.IsTypeOf (wellKnownType);
 
+		[MethodImpl(AggressiveInlining)]
 		public string GetDisplayName () => Type.GetDisplayName ();
 
+		[MethodImpl(AggressiveInlining)]
 		public override string ToString () => Type.ToString ();
 	}
 }
