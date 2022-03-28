61115f616112f2b7112bf5547c2a2fb6f92931ab
Jackson Schuster <jschuster@microsoft.com>
2022-03-28 11:50:17 -0700

    Add implicit interface implementation case

--- test/Mono.Linker.Tests.Cases/Libraries/RootLibrary.cs
+++ test/Mono.Linker.Tests.Cases/Libraries/RootLibrary.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
@@ -10,6 +12,7 @@ namespace Mono.Linker.Tests.Cases.Libraries
 {
 	[SetupLinkerArgument ("-a", "test.exe", "library")]
 	[SetupLinkerArgument ("--enable-opt", "ipconstprop")]
+	[SetupLinkerArgument ("--skip-unresolved")]
 	[VerifyMetadataNames]
 	public class RootLibrary
 	{
@@ -189,7 +192,72 @@ namespace Mono.Linker.Tests.Cases.Libraries
 			object IEnumerator.Current { [Kept] get { throw new PlatformNotSupportedException (); } }
 
 			[Kept]
-			void IEnumerator.Reset () {}
+			void IEnumerator.Reset () { }
+		}
+
+		[Kept]
+		[KeptInterface (typeof (ICollection<int>))]
+		[KeptInterface (typeof (IEnumerable<int>))]
+		[KeptInterface (typeof (IEnumerable))]
+		public class UninstantiatedPublicClassWithImplicitlyImplementedInterface : ICollection<int>
+		{
+			[Kept]
+			[KeptBackingField]
+			public int Count { [Kept] get; }
+
+			[Kept]
+			[KeptBackingField]
+			public bool IsReadOnly { [Kept] get; }
+
+			internal UninstantiatedPublicClassWithImplicitlyImplementedInterface () { }
+
+			[Kept]
+			public void CopyTo (Array array, int index)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[Kept]
+			public void Add (int item)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[Kept]
+			public void Clear ()
+			{
+				throw new NotImplementedException ();
+			}
+
+			[Kept]
+			public bool Contains (int item)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[Kept]
+			public void CopyTo (int[] array, int arrayIndex)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[Kept]
+			public bool Remove (int item)
+			{
+				throw new NotImplementedException ();
+			}
+
+			[Kept]
+			public IEnumerator GetEnumerator ()
+			{
+				throw new NotImplementedException ();
+			}
+
+			[Kept]
+			IEnumerator<int> IEnumerable<int>.GetEnumerator ()
+			{
+				throw new NotImplementedException ();
+			}
 		}
 
 		[Kept]
@@ -229,5 +297,91 @@ namespace Mono.Linker.Tests.Cases.Libraries
 		internal void Unused ()
 		{
 		}
+	}
+	
+	[Kept]
+	[KeptInterface (typeof (ICollection<IPAddressInformation>))]
+	[KeptInterface (typeof (IEnumerable<IPAddressInformation>))]
+	[KeptInterface (typeof (IEnumerable))]
+	[KeptAttributeAttribute (typeof (System.Reflection.DefaultMemberAttribute))]
+	public class IPAddressInformationCollection : ICollection<IPAddressInformation>
+	{
+		[Kept]
+		private readonly List<IPAddressInformation> _addresses = new List<IPAddressInformation> ();
+
+		internal IPAddressInformationCollection ()
+		{
+		}
+
+		[Kept]
+		public virtual void CopyTo (IPAddressInformation[] array, int offset)
+		{
+			_addresses.CopyTo (array, offset);
+		}
+
+		[Kept]
+		public virtual int Count {
+		[Kept]
+			get {
+				return _addresses.Count;
+			}
+		}
+
+		[Kept]
+		public virtual bool IsReadOnly {
+		[Kept]
+			get {
+				return true;
+			}
+		}
+
+		[Kept]
+		public virtual void Add (IPAddressInformation address)
+		{
+			throw new NotSupportedException ();
+		}
+
+		internal void InternalAdd (IPAddressInformation address)
+		{
+			_addresses.Add (address);
+		}
+
+		[Kept]
+		public virtual bool Contains (IPAddressInformation address)
+		{
+			return _addresses.Contains (address);
+		}
+
+		[Kept]
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator ()
+		{ return this.GetEnumerator (); }
+		[Kept]
+		public virtual IEnumerator<IPAddressInformation> GetEnumerator () { return _addresses.GetEnumerator (); }
+		[Kept]
+		public virtual IPAddressInformation this[int index] {
+		[Kept]
+			get {
+				return _addresses[index];
+			}
+		}
+
+		[Kept]
+		public virtual bool Remove (IPAddressInformation address)
+		{
+			throw new NotSupportedException ();
+		}
+
+		[Kept]
+		public virtual void Clear ()
+		{
+			throw new NotSupportedException ();
+		}
+	}
+	[Kept]
+	public class IPAddressInformation
+	{
+		[Kept]
+		public IPAddressInformation () { }
+
 	}
 }
