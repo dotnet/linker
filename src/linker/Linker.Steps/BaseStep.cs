57dd6679f904948011d5ec1c7c24473ba0febe13
Jackson Schuster <jschuster@microsoft.com>
2022-06-08 14:50:45 +0000

    Formatting

--- src/linker/Linker.Steps/BaseStep.cs
+++ src/linker/Linker.Steps/BaseStep.cs
@@ -29,7 +29,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using System;
 using System.Diagnostics;
 using Mono.Cecil;
 
--- src/linker/Linker/Driver.cs
+++ src/linker/Linker/Driver.cs
@@ -34,7 +34,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.Loader;
 using System.Text;
