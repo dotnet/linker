399168ee03eee0f22fe0bb65bf4374da97aeb6b1
Jackson Schuster <jschuster@microsoft.com>
2022-09-22 10:35:39 -0500

    Update ParameterIndex comment with more why explanation

--- src/ILLink.Shared/TypeSystemProxy/NonThisParameterIndex.cs
+++ src/ILLink.Shared/TypeSystemProxy/NonThisParameterIndex.cs
@@ -7,6 +7,7 @@ namespace ILLink.Shared
 	/// Used to indicate the index of the parameter in source code (i.e. the index is not offset by 1 if there is a `this` parameter)
 	/// This enum and <see cref="ILLink.Shared.ILParameterIndex"/> is used to enforce a differentiation between scenarios where the 0
 	/// index should be `this` and when the 0 index should be the first non-this parameter in the type system.
+	/// Using an int for both of these scenarios can easily cause array bound exceptions, so extensions using one of these enums should be used whenever possible.
 	/// There are no named enum values, the underlying integer value represents the index value.
 	/// There is no way to represent a `this` parameter with a NonThisParameterIndex.
 	/// See IMethodSymbolExtensions and MethodReferenceExtensions for helper methods to avoid indexing the Parameters properly directly with ints
