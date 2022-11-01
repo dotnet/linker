3d490af22b2092b4a2962924ad6eb59af75c0188
Jackson Schuster <jschuster@microsoft.com>
2022-11-01 11:16:45 -0700

    Add link to issue related to base method sweeping

--- test/Mono.Linker.Tests.Cases/Inheritance.VirtualMethods/VirtualMethodGetsStrippedIfImplementingMethodGetsInvokedDirectly.cs
+++ test/Mono.Linker.Tests.Cases/Inheritance.VirtualMethods/VirtualMethodGetsStrippedIfImplementingMethodGetsInvokedDirectly.cs
@@ -13,6 +13,7 @@ namespace Mono.Linker.Tests.Cases.Inheritance.VirtualMethods
 		class B
 		{
 			// TODO: Would be nice to be removed
+			// https://github.com/dotnet/linker/issues/3097
 			[KeptBy (typeof (A), nameof (A.Foo), "BaseMethod")]
 			public virtual void Foo ()
 			{
