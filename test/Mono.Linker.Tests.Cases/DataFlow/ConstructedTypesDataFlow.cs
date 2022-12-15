1ea8287472005e21c6ee7cb4834fba892d14c205
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2022-12-15 03:58:35 -0800

    More tests

--- test/Mono.Linker.Tests.Cases/DataFlow/ConstructedTypesDataFlow.cs
+++ test/Mono.Linker.Tests.Cases/DataFlow/ConstructedTypesDataFlow.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,16 +23,69 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 
 		class DeconstructedVariable
 		{
-			[ExpectedWarning("IL2077")]
+			// https://github.com/dotnet/linker/issues/3158
+			[ExpectedWarning ("IL2077", ProducedBy = ProducedBy.Trimmer | ProducedBy.NativeAot)]
 			static void DeconstructVariableNoAnnotation ((Type type, object instance) input)
 			{
 				var (type, instance) = input;
 				type.RequiresPublicMethods ();
 			}
 
+			record TypeAndInstance ([DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)] Type type, object instance);
+
+			// This is a tricky one - in a way this is a compiler bug
+			// Even though the record's property is declared with the annotation, the generated Deconstruct doesn't
+			// propagate the annotation into the out parameters.
+			// So analyzer would see the annotation (since it doesn't see the Deconstruct call - I think)
+			// But IL tooling will see a problem since it sees the Deconstruct call.
+			[ExpectedWarning ("IL2067", ProducedBy = ProducedBy.Trimmer | ProducedBy.NativeAot)]
+			static void DeconstructRecordWithAnnotation (TypeAndInstance value)
+			{
+				var (type, instance) = value;
+				type.RequiresPublicMethods ();
+			}
+
+			class TypeAndInstanceManual
+			{
+				[DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)]
+				public Type type;
+				public object instance;
+
+				public TypeAndInstanceManual ([DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)] Type type, object instance)
+					=> (this.type, this.instance) = (type, instance);
+
+				public void Deconstruct ([DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)] out Type type, out object instance)
+					=> (type, instance) = (this.type, this.instance);
+			}
+		
+			// This case actually works because the annotation is correctly propagated through the Deconstruct
+			static void DeconstructClassWithAnnotation (TypeAndInstanceManual value)
+			{
+				var (type, instance) = value;
+				type.RequiresPublicMethods ();
+			}
+
+			record TypeAndInstanceRecordManual ([DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)] Type type, object instance)
+			{
+				// The generated property getter doesn't have the same attributes???
+				// The attributes are only propagated to the generated .ctor - so suppressing the warning the this.type doesn't have the matching annotations
+				[UnconditionalSuppressMessage ("", "IL2072")]
+				public void Deconstruct ([DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)] out Type type, out object instance)
+					=> (type, instance) = (this.type, this.instance);
+			}
+
+			static void DeconstructRecordManualWithAnnotation (TypeAndInstanceRecordManual value)
+			{
+				var (type, instance) = value;
+				type.RequiresPublicMethods ();
+			}
+
 			public static void Test ()
 			{
 				DeconstructVariableNoAnnotation ((typeof (string), null));
+				DeconstructRecordWithAnnotation (new (typeof (string), null));
+				DeconstructClassWithAnnotation (new (typeof (string), null));
+				DeconstructRecordManualWithAnnotation (new (typeof (string), null));
 			}
 		}
 	}
